effd4846ecfdb8331961edab4f907c4545959b97
Nat Wilson <nwilson@sfu.ca>
2011-11-17 12:26:28 -0800

    Began adding projection and filtering methods to gpxparser.py

--- vector/gpxparser.py
+++ vector/gpxparser.py
@@ -6,7 +6,14 @@ http://the.taoofmac.com/space/blog/2005/10/11/2359
 """
 
 import sys
+import numpy as np
 from xml.dom import minidom, Node
+
+try:
+    from pyproj import Proj
+except ImportError:
+    # No projection capabilities
+    pass
 
 class GPXParser:
     """ Used for reading out track data from the GPX files that can be
@@ -14,6 +21,7 @@ class GPXParser:
     def __init__(self, filename):
         self.filename = filename
         self.tracks = {}
+        self.has_projector = False
         try:
             doc = minidom.parse(filename)
             doc.normalize()
@@ -22,6 +30,23 @@ class GPXParser:
         gpx = doc.documentElement
         for node in gpx.getElementsByTagName('trk'):
             self.ParseTrack(node)
+
+    def project(self, **kwargs):
+        """ Wrapper for libproj.4 Proj class. """
+        try:
+            self.Projector = Proj(**kwargs)
+            self.has_projector = True
+        except NameError:
+            raise ImportError("no pyproj found")
+            return
+        self.tracks_proj = {}
+        for name in self.list():
+            for t in self.tracks[name].keys():
+                lon = self.track[name][t]['lon']
+                lat = self.track[name][t]['lat']
+                ele = self.track[name][t]['ele']
+                x, y = self.Projector(lon, lat)
+                self.tracks_proj[name][t] = {'x':x, 'y':y, 'ele',ele}
 
     def parsetrack(self, trk):
         name = trk.getElementsByTagName('name')[0].firstChild.data
@@ -40,11 +65,19 @@ class GPXParser:
         return [str(i) for i in self.tracks.keys()]
 
     def gettrack(self, name):
-        """ Return a list of triples containing track coordinates, sorted by
-        time, for track with name. """
+        """ Return a list of triples containing track coordinates
+        sorted by time for track with name. """
         times = self.tracks[name].keys()
         times.sort()
-        points = [self.tracks[name][time] for time in times]
+        points = [self.tracks[name][t] for t in times]
+        return [(point['lon'],point['lat'],point['ele']) for point in points]
+
+    def getxyz(self, name):
+        """ Return a list of triples containing track projected
+        coordinates, sorted by time, for track with name. """
+        times = self.tracks_proj[name].keys()
+        times.sort()
+        points = [self.tracks_proj[name][t] for t in times]
         return [(point['lon'],point['lat'],point['ele']) for point in points]
 
     def gettimes(self, name):
@@ -57,7 +90,31 @@ class GPXParser:
         """ Return a guppy.Polyline containing all tracks. """
         pass
 
+    def filter_by_displacement(self, name, threshold, proj_kwargs=None):
+        """ Return a list of lists, where each sublist contains consecutive GPS
+        points between which the displacement is less than threshold. """
+        # Make sure coordinates are projected
+        if not self.has_projector:
+            if proj_kwargs is not None:
+                self.project(**proj_kwargs)
+            else:
+                raise ProjectionError("Need to define proj_kwargs dictionary")
+                return
+        xyz = self.gettrack_proj(name)
+        X = np.array([i[0] for i in xyz])
+        Y = np.array([i[1] for i in xyz])
+        Z = np.array([i[2] for i in xyz])
+        DIST = np.sqrt((X[:-1]-X[1:])**2 + (Y[:-1]-Y[1:])**2)
+        #[INCOMPLETE - NEED TO FILTER BASED ON DIST]
+
     def split_natural_breaks(self, name):
         """ Split a track into linear natural segments, with nodes where
         direction changes. """
         pass
+
+
+class ProjectionError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return self.value
