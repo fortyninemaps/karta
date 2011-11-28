1ae1bece3e0580704465b080b2abf21ecd225f07
Nat Wilson <nwilson@sfu.ca>
2011-11-28 11:40:06 -0800

    experimental stats module added to vector package with (slow) variogram estimator

--- __init__.py
+++ __init__.py
@@ -3,8 +3,10 @@ geo_tools is a collection of python modules for handling vector and raster
 geospatial data.
 """
 
+import vector
 from vector.guppy import  *
 from vector import shapefile
+import raster
 from raster.aaigrid_driver import AAIGrid
 
 #__all__ = ["vector", "vector.guppy", "raster.aaigrid_driver"]
