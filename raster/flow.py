ee23042f6cd50f92f1e04e27f4aa7ace8d50384d
Nat Wilson <nwilson@sfu.ca>
2012-01-22 18:17:30 -0800

    flow direction in raster.flow is running, although not correctly or quickly

--- raster/flow.py
+++ raster/flow.py
@@ -12,42 +12,105 @@ def dinfinity(D, res=(30.0,30.0)):
     dx = res[0]
     dy = res[1]
 
-    def get_facet_slope(e0, e1, e2, d1, d2):
+    #def get_facet_slope(e0, e1, e2, d1, d2, ac, af):
+        #""" Calculate facet slope, where e0 is the central node, e1 is
+        #a node in a cardinal direction, and e2 is a node in a diagonal.
+        #"""
+        ##print e0,e1,e2, "\t", d1,d2, "\t", ac,af
+        #pi = math.pi
+        #s1 = (e0 - e1) / d1
+        #s2 = (e1 - e2) / d2
+        #try:
+            #aspect = math.atan(s2 / s1)
+        #except ZeroDivisionError:
+            #aspect = 0.25 * pi
+        #magnitude = math.sqrt(s1*s1 + s2*s2)
+
+        #if aspect > 0.25*pi:
+            #aspect = 0.25*pi
+            #magnitude = abs((e0 - e2) / math.sqrt(d1*d1 + d2*d2))
+        #elif aspect < -0.25*pi:
+            #aspect = -0.25*pi
+            #magnitude = abs((e0 - e2) / math.sqrt(d1*d1 + d2*d2))
+
+        ## Rotate / flip facet accordng to ac, af
+        #aspect_corr = af * aspect + ac * 0.5*pi
+        ##aspect_corr = aspect
+
+        ##print "\t", magnitude, aspect_corr
+        #return magnitude, aspect_corr
+
+    def get_facet_slope(e0, e1, e2, d1, d2, ac, af):
         """ Calculate facet slope, where e0 is the central node, e1 is
         a node in a cardinal direction, and e2 is a node in a diagonal.
         """
-        s1 = (e0 - e1) / d1
-        s2 = (e1 - e2) / d2
-        aspect = math.atan(s2 / s1)
-        magnitude = math.sqrt(s1*s1 + s2*s2)
-        return magnitude, aspect
+        pi = math.pi
+
+        u1 = (0.0, d1, e1-e0)
+        u2 = (d2, 0.0, e2-e1)
+        n = -np.cross(u1, u2)
+        nhat = n / np.linalg.norm(n)
+        nhat[nhat==0.0] = 0.0
+        aspect = math.atan(nhat[0] / nhat[1])
+        slope = math.acos(nhat[2])
+
+        if nhat[1] < 0:
+            aspect += pi
+
+        # For aspects that point backward, flip the sign of slope
+        #if aspect > 0.5*pi and aspect <= 1.5*pi:
+            #slope = -slope
+
+        # Rotate / flip facet accordng to ac, af
+        aspect_corr = af * aspect + ac * 0.5*pi
+
+        # Make sure aspect is in the range [0, 2pi)
+        while aspect_corr >= 2*pi:
+            aspect_corr -= 2*pi
+
+        return slope, aspect_corr
 
     def get_flow_dir(A, dx, dy):
         """ For a 3x3 sector A, compute the flow direction, or return
         nan to indicate an unresolved case.
         """
-        cells1 = [(0,0), (0,1), (0,2),
-                  (1,0),        (1,2),
-                  (2,0), (2,1), (2,2)]
-        cells2 = [(0,1), (0,2), (1,2),
-                  (0,0),        (2,2),
-                  (1,0), (2,0), (2,1)]
-        d1 = [dx, dx, dy,
+        e1 = [(0,1), (0,1), (1,2),
+              (1,0),        (1,2),
+              (1,0), (2,1), (2,1)]
+        e2 = [(0,0), (0,2), (0,2),
+              (0,0),        (2,2),
+              (2,0), (2,0), (2,2)]
+        d1 = [dy, dy, dx,
+              dx,     dx,
+              dx, dy, dy]
+        d2 = [dx, dx, dy,
               dy,     dy,
               dy, dx, dx]
-        d2 = math.sqrt(dx*dx + dy*dy)
+        ac = [0, 0, 1,              # Translation table
+              -1,   1,
+              -1, -2, 2]
+        af = [-1, 1, -1,            # Flipping table
+              1,      1,
+              -1, 1, -1]
 
         # Compute all slopes
-        fs = map(get_facet_slope, [A[1,1] for c in cells1],
-                                  [A[c] for c in cells1],
-                                  [A[c] for c in cells2],
-                                  d1, [d2 for i in d1])
+        fs = map(get_facet_slope, [A[1,1] for _e in e1],
+                                  [A[_e] for _e in e1],
+                                  [A[_e] for _e in e2],
+                                  d1, d2, ac, af)
 
-        # Return the direction of the most negative, or return nan
-        pdb.set_trace()
+        FS = np.array([[fs[0][0], fs[1][0], fs[2][0]],
+                       [fs[3][0], 0.0,   fs[4][0]],
+                       [fs[5][0], fs[6][0], fs[7][0]]])
+        pi = math.pi
+        AS = np.array([[fs[0][1]/pi, fs[1][1]/pi, fs[2][1]/pi],
+                       [fs[3][1]/pi, 0.0,         fs[4][1]/pi],
+                       [fs[5][1]/pi, fs[6][1]/pi, fs[7][1]/pi]])
+
+        # Return the direction of the steepest slope, or return 0.0
         fs.sort()
         fs.reverse()
-        return fs[0][0] < 0 and fs[0][1] or np.nan
+        return fs[-1][0] > 0 and fs[-1][1] or 0.0
 
 
     # For each cell in D[1:-1, 1:-1], find the flow direction
@@ -58,7 +121,7 @@ def dinfinity(D, res=(30.0,30.0)):
                   [D[:-2,:-2], D[1:-1,:-2], D[2:,:-2]],
                   [D[:-2,1:-1], D[1:-1,1:-1], D[2:,1:-1]],
                   [D[:-2,2:], D[1:-1,2:], D[2:,2:]],
-                  ]).transpose([2,3,0,1])
+                  ]).transpose([2,3,1,0])
 
     #get_flow_dir_v = np.vectorize(get_flow_dir)
 
--- tests/flow_tests.py
+++ tests/flow_tests.py
@@ -5,16 +5,22 @@ sys.path.append("../")
 from numpy import *
 import matplotlib.pyplot as plt
 import raster.flow as flow
+import raster.raster as raster
 
-X, Y = meshgrid(range(100), range(100))
+#X, Y = meshgrid(range(250), range(250))
 
-D = 1.1 * X + 0.8 * Y
+#D = 0.0 * X + 0.5 * Y
+#D = 0.5 * X + 0.0 * Y
+#D = 0.5 * X + 0.5 * Y
+D = raster.witch_of_agnesi(nx=75, ny=75)
 
 upstream_area = flow.dinfinity(D)
 
 plt.imshow(upstream_area)
 
 #plt.imshow(D)
+
+print upstream_area/pi
 
 plt.show()
 
