f91efbd69149fd03046f0e7f207e3150ba20bd88
Nat Wilson <nwilson@sfu.ca>
2012-01-23 17:42:30 -0800

    started reimplementing d-infinity from MATLAB code

--- raster/flow.py
+++ raster/flow.py
@@ -1,18 +1,59 @@
 """
 Flow-routing algorithms
+
+
+The following license applies to:
+    pixel_flow, facet_flow, upslope_area
+
+    Copyright (c) 2009, The MathWorks, Inc.
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or
+    without modification, are permitted provided that the following
+    conditions are met:
+
+        * Redistributions of source code must retain the above copyright
+          notice, this list of conditions and the following disclaimer.
+
+        * Redistributions in binary form must reproduce the above copyright
+          notice, this list of conditions and the following
+          disclaimer in the documentation and/or other materials
+          provided with the distribution
+
+        * Neither the name of the The MathWorks, Inc. nor the names
+          of its contributors may be used to endorse or promote
+          products derived from this software without specific prior
+          written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+    CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+    USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+    AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+    IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+    THE POSSIBILITY OF SUCH DAMAGE.
+
 """
 
+#import sys
 import raster
 import math
 import numpy as np
-import pdb
+import pdb, time
 
 def dinfinity(D, res=(30.0,30.0)):
     """ Infinite flow direction scheme of Tarboton (1997). """
+    t0 = time.time()
     dx = res[0]
     dy = res[1]
 
-    #def get_facet_slope(e0, e1, e2, d1, d2, ac, af):
+    def get_facet_slope_old(e0, e1, e2, d1, d2, ac, af):
         #""" Calculate facet slope, where e0 is the central node, e1 is
         #a node in a cardinal direction, and e2 is a node in a diagonal.
         #"""
@@ -39,6 +80,7 @@ def dinfinity(D, res=(30.0,30.0)):
 
         ##print "\t", magnitude, aspect_corr
         #return magnitude, aspect_corr
+        return
 
     def get_facet_slope(e0, e1, e2, d1, d2, ac, af):
         """ Calculate facet slope, where e0 is the central node, e1 is
@@ -57,9 +99,11 @@ def dinfinity(D, res=(30.0,30.0)):
         if nhat[1] < 0:
             aspect += pi
 
-        # For aspects that point backward, flip the sign of slope
-        #if aspect > 0.5*pi and aspect <= 1.5*pi:
-            #slope = -slope
+        # For aspects that point inward, flip the sign of slope
+        if aspect < 0:
+            aspect += 2.0*pi
+        if aspect > 0.5*pi and aspect <= 1.5*pi:
+            slope = -slope
 
         # Rotate / flip facet accordng to ac, af
         aspect_corr = af * aspect + ac * 0.5*pi
@@ -67,12 +111,15 @@ def dinfinity(D, res=(30.0,30.0)):
         # Make sure aspect is in the range [0, 2pi)
         while aspect_corr >= 2*pi:
             aspect_corr -= 2*pi
+        while aspect_corr < 0.0:
+            aspect_corr += 2*pi
 
         return slope, aspect_corr
 
     def get_flow_dir(A, dx, dy):
-        """ For a 3x3 sector A, compute the flow direction, or return
-        nan to indicate an unresolved case.
+        """ For a 3x3 sector A, compute the direction of maximum outward
+        slope (assumed to be the flow direction). Returns nan when
+        indeterminate.
         """
         e1 = [(0,1), (0,1), (1,2),
               (1,0),        (1,2),
@@ -99,146 +146,218 @@ def dinfinity(D, res=(30.0,30.0)):
                                   [A[_e] for _e in e2],
                                   d1, d2, ac, af)
 
-        FS = np.array([[fs[0][0], fs[1][0], fs[2][0]],
-                       [fs[3][0], 0.0,   fs[4][0]],
-                       [fs[5][0], fs[6][0], fs[7][0]]])
-        pi = math.pi
-        AS = np.array([[fs[0][1]/pi, fs[1][1]/pi, fs[2][1]/pi],
-                       [fs[3][1]/pi, 0.0,         fs[4][1]/pi],
-                       [fs[5][1]/pi, fs[6][1]/pi, fs[7][1]/pi]])
-
-        # Return the direction of the steepest slope, or return 0.0
+        # Return the direction of the steepest inward slope, or return 0.0
         fs.sort()
-        fs.reverse()
-        return fs[-1][0] > 0 and fs[-1][1] or 0.0
+        return fs[0][0] < 0 and fs[0][1] or np.nan
 
 
     # For each cell in D[1:-1, 1:-1], find the flow direction
-    #   First, associate a 3x3 matrix with each cell, composed of neighbouring cells
-    #   Next, apply a vectorized routine to do the calculation
+    print "Calculating flow directions..."
 
+    # Associate a 3x3 matrix with each cell, composed of neighbouring cells
     D3 = np.array([
                   [D[:-2,:-2], D[1:-1,:-2], D[2:,:-2]],
                   [D[:-2,1:-1], D[1:-1,1:-1], D[2:,1:-1]],
                   [D[:-2,2:], D[1:-1,2:], D[2:,2:]],
                   ]).transpose([2,3,1,0])
 
-    #get_flow_dir_v = np.vectorize(get_flow_dir)
 
-    flow_dir = np.zeros_like(D[1:-1,1:-1])
+    flow_field = np.zeros_like(D[1:-1,1:-1])
     for i in range(D3.shape[0]):
         for j in range(D3.shape[1]):
-            flow_dir[i,j] = get_flow_dir(D3[i,j,:,:], dx, dy)
+            flow_field[i,j] = get_flow_dir(D3[i,j,:,:], dx, dy)
 
-    return flow_dir         # temporary
-
-    #flow_dir = get_flow_dir_v(D3, dx*np.ones_like(D[1:-1,1:-1]),
+    #get_flow_dir_v = np.vectorize(get_flow_dir)
+    #flow_field = get_flow_dir_v(D3, dx*np.ones_like(D[1:-1,1:-1]),
                                   #dy*np.ones_like(D[1:-1,1:-1]))
 
+    print "\tdone"
+    #return flow_field         # temporary
 
     # Upstream calculation
-    def upstream(fd, area, i, j):
+    def upstream(ff, area, i, j):
         """ Recursive function for computing upstream area at (i,j),
         given an array to write to and a flow field.
         """
-
         def proportion(alpha, position):
             """ Return proportion flowing to center from position given
-            angle alpha, and where position is
+            flow angle alpha, and where position is
                             0   1   2
                             3   *   4
                             5   6   7
             """
             pi = math.pi
 
-            if position == 0:
-                pass
-            elif position == 1:
-                alpha -= 0.25*pi
-            elif position == 2:
-                alpha -= 0.5*pi
-            elif position == 3:
-                alpha += 0.25*pi
-            elif position == 4:
-                alpha += 0.75*pi
-            elif position == 5:
-                alpha -= 0.5*pi
-            elif position == 6:
-                alpha -= 0.75*pi
-            elif position == 7:
-                alpha += pi
+            # Beta is the direction to the centre node
+            BETA = [0.75, 1.0, 1.25, 0.5, 1.5, 0.25, 0.0, 1.75]
+            beta = BETA[position] * pi
 
-            if alpha > 0.25*pi:
-                alpha = 0.5*pi - alpha
-            if alpha < 0:
-                alpha = 0
+            theta = abs(beta-alpha)
+            return theta <= 0.25*pi and (0.25*pi-theta) / (0.25*pi) or 0.0
 
-            return alpha / 0.25*pi
+        if not np.isnan(UA[i,j]):
 
-        if UA[i,j] is not np.nan:
             return UA[i,j]
 
         else:
+
             cumarea = area
 
-            fdij = [fd[i-1, j-1],   fd[i-1, j],     fd[i-1, j+1],
-                    fd[i, j-1],                     fd[i, j+1],
-                    fd[i+1, j-1],   fd[i+1, j],     fd[i+1, j+1]]
+            I = [i-1, i-1, i-1,
+                 i,        i,
+                 i+1, i+1, i+1]
+            J = [j-1, j, j+1,
+                 j-1,    j+1,
+                 j-1, j, j+1]
 
-            # Test whether the surrounding cells flow into (i,j)
-            for position, angle in enumerate(fdij):
-                cumarea += proportion(angle, position) * upstream(fd, k, l)
+            try:
+                ffij = [ff[i-1, j-1],   ff[i-1, j],     ff[i-1, j+1],
+                        ff[i, j-1],                     ff[i, j+1],
+                        ff[i+1, j-1],   ff[i+1, j],     ff[i+1, j+1]]
+            except IndexError:
+                return 0.0
+
+            iffij = enumerate(ffij)
+
+            # Retains cells where p > 0.0
+            contributors = filter(
+                    lambda a: proportion(a[1], a[0]) > 0.0, iffij)
+
+            # For each contributor, add the upslope area
+            for a in contributors:
+                try:
+                    cumarea += proportion(a[1], a[0]) * upstream(ff, area, I[a[0]], J[a[0]])
+                except IndexError:
+                    pass
+
+            UA[i,j] = cumarea
 
             return cumarea
 
+
     # Calculate upstream areas
+    print "Calculating upstream areas..."
     global UA
 
     UA = np.nan * np.zeros_like(D)
     area = res[0] * res[1]
 
-    for i in range(1, D.shape[0]-1):
-        for j in range(1, D.shape[1]-1):
+    for i in range(1, D.shape[0]):
+        for j in range(1, D.shape[1]):
+            if np.isnan(UA[i,j]):
+                upstream(flow_field, area, i, j)
 
-            if UA[i,j] is not np.nan:
+    print "\tdone"
 
-                upstream(flow_dir, area, i, j)
+    print "time:", time.time() - t0
 
-    return UA
-
-
-
-
+    return raster.pad(UA)
 
 
-def d8_mult(D):
-    """ Multiple flow direction D8 algorithm.
+def facet_flow(e0, e1, e2, d1=30.0, d2=30.0):
+    """ Return flow direction and slope for an east-northeast grid
+    facet.
 
-    Described by Quinn et al. (1991).
+                        e2
+                        |
+                        |
+                        | d2
+                        |
+                        |
+            e0 -------- e1
+                  d1
+
+    Based on Tarboton (1997).
+    Originally implemented in MATLAB by Steven L. Eddins.
     """
-    F = np.zeros_like(D)
-    dxy = math.sqrt(dx8dx+dy*dy)
 
-    # Calculate gradients
-    dzdx = (D[:,1:] - D[:,:-1]) / dx
-    dzdy = (D[1:,:] - D[:-1,:]) / dy
-    dzd1 = (D[1:,1:] - D[:-1,:-1]) / dxy
-    dzd2 = (D[1:,:-1] - D[:-1,1:]) / dxy
+    s1 = (e0 - e1) / d1             # Equation (1)
+    s2 = (e1 - e2) / d2             # Equation (2)
 
-    # Define a recursive function to calculate upstream area
-    def upstream():
-        pass
+    r = math.atan2(s2, s1)        # Equation (3)
+    s = math.sqrt(s1*s1 + s2*s2)    # Equation (3)
 
+    # Constrain flow direction to lie within or along the edges of the facet
+    r, s = r > 0.0 and (r, s) or (0.0, s1)
 
-    # Vectorize
-    upstreamv = np.vectorize(upstream)
+    diagonal_angle = math.atan2(d2, d1)
+    diagonal_distance = math.sqrt(d1*d1 + d2*d2)
+    r, s = r < diagonal_angle and (r, s) or (diagonal_angle, (e0 - e2) / diagonal_distance)
 
-    # Apply to D
-    F = upstreamv(D)
+    return r, s
 
 
 
-    return F
+def pixel_flow(E, i, j, d1=30.0, d2=30.0):
+    """ Downslope flow direction for DEM pixels.
+
+    Originally implemented by Steven Eddins (2007)
+    """
+
+    def border_nans(A):
+        """ Return indices of nans that touch the border. """
+        raise Exception("border_nans not implemented")
+        nans = np.isnan(A)
+
+
+    m, n = E.shape
+
+    # Preprocess NaNs connected to the border.
+    #highest_value = E[np.isnan(E)==False].max()
+    #bump = 1.0
+    #E[border_nans(E)] = highest_value + bump
+
+    # Compute linear indices at desired locations
+    e0_idx = j * m + i
+
+    # Table 1, page 311
+    # Row and column offsets corresponding to e1 and e2 for each entry
+    e1_row_offsets = [0, -1, -1,  0,  0,  1,  1,  0]
+    e1_col_offsets = [1,  0,  0, -1, -1,  0,  0,  1]
+
+    e2_row_offsets = [-1, -1, -1, -1,  1,  1,  1,  1]
+    e2_col_offsets = [ 1,  1, -1, -1, -1, -1,  1,  1]
+
+    # Linear e1 and e2 offsets
+    e1_linear_offsets = e1_col_offsets * m + e1_row_offsets
+    e2_linear_offsets = e2_col_offsets * m + e1_row_offsets
+
+    # Initialize R and S based on the first facet
+    Eflat = E.flatten()
+    E0 = Eflat[e0_idx]
+    try:
+        E1 = Eflat[e0_idx + e1_linear_offsets[0] - 1]
+        E2 = Eflat[e0_idx + e2_linear_offsets[0] - 1]
+    except:
+        pdb.set_trace()
+
+    R, S = facet_flow(E0, E1, E2, d1, d2)
+
+    # Multipliers ac and af used to convert from east-northeast facet
+    # to any other fact (using Eq. 6)
+    ac = [0,  1,  1,  2,  2,  3,  3,  4]
+    af = [1, -1,  1, -1,  1, -1,  1, -1]
+
+    if S > 0:
+        print "hooray!"
+        R = (af[0] * R) + (ac[0] * math.pi / 2.0)
+    else:
+        R = np.nan
+    #R = S > 0 and (af[0] * R) + (ac[0] * math.pi / 2.0) or np.nan
+
+    # Compute Rk and Sk corresponding to the k-th facet
+    # Where Sk>0 and Sk>S, S=Sk and recompute R(Rk)
+    for k in range(1,8):
+        E1 = Eflat[e0_idx + e1_linear_offsets[k] - 1]
+        E2 = Eflat[e0_idx + e2_linear_offsets[k] - 1]
+        Rk, Sk = facet_flow(E0, E1, E2, d1, d2)
+
+        S = max(S, Sk)
+
+        if (Sk > S) * (Sk > 0.0):
+            R = (af[k] * Rk) + (ac[k] * math.pi / 2.0)
+
+    return R, S
 
 
 
--- tests/flow_tests.py
+++ tests/flow_tests.py
@@ -6,21 +6,36 @@ from numpy import *
 import matplotlib.pyplot as plt
 import raster.flow as flow
 import raster.raster as raster
+import pdb
+
 
 #X, Y = meshgrid(range(250), range(250))
 
 #D = 0.0 * X + 0.5 * Y
 #D = 0.5 * X + 0.0 * Y
 #D = 0.5 * X + 0.5 * Y
-D = raster.witch_of_agnesi(nx=75, ny=75)
+D = raster.witch_of_agnesi(nx=75, ny=75, a=10)
 
-upstream_area = flow.dinfinity(D)
+Dp = raster.pad(D)
+c = [(i,j) for i in range(1, Dp.shape[0]-1) for j in range(1, Dp.shape[1]-1)]
 
-plt.imshow(upstream_area)
+ff = map(lambda a: flow.pixel_flow(Dp, a[0], a[1], d1=20.0, d2=20.0), c)
 
-#plt.imshow(D)
+#pdb.set_trace()
+R = array([i[0] for i in ff]).reshape(D.shape)
+S = array([i[1] for i in ff]).reshape(D.shape)
 
-print upstream_area/pi
+#pdb.set_trace()
 
-plt.show()
+#plt.imshow(S)
+
+#upstream_area = flow.dinfinity(D)
+
+#plt.imshow(upstream_area, cmap="gray")
+#plt.ylim([0,75])
+#plt.xlim([0,75])
+
+#print upstream_area/pi
+
+#plt.show()
 
