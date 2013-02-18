8f40e9f5c972b9a80b7e98f8743446c03bbd1731
Nat Wilson <nwilson@sfu.ca>
2013-02-17 17:18:35 -0800

    renamed geojson and vtk writers, because these should be accessed though guppy
    --HG--
    rename : vector/geojson.py => vector/_geojson.py
    rename : vector/vtk.py => vector/_vtk.py

---
+++ vector/_geojson.py
@@ -0,0 +1,181 @@
+""" This module includes the `GeoJSON` class and convenience functions for
+dealing with GeoJSON data. The `GeoJSON` class uses the builtin json module and
+enforces GeoJSON standards. """
+
+import guppy
+import json
+import traceback
+
+__status__ = 'incomplete'
+
+class GeoJSONWriter:
+    """ Object for converting guppy objects to GeoJSON strings.
+    Subclasses must implement:
+        - a data writer
+        - calc_bbox()
+    as well as add an attribute 'strtype'
+
+    DOES NOT HANDLE FEATURE COLLECTIONS
+    """
+    supobj = {}
+
+    def __init__(self, gpobj, fnm, crs_urn=None, bbox=None):
+        type_equiv = {guppy.Point:'Point',
+                      guppy.Multipoint:'MultiPoint',
+                      guppy.Line:'LineString',
+                      guppy.Polygon:'Polygon'}
+
+        if type(gpobj) in type_equiv:
+            self.typestr = type_equiv[type(gpobj)]
+        else:
+            raise NotImplementedError('input object not a recognizes guppy '
+                                      'type')
+        self.gpobj = gpobj
+        if crs_urn is not None:
+            self.add_named_crs(crs_urn)
+        if self.typestr != 'Point':
+            self.supobj['type'] = 'Feature'
+            self.add_bbox(bbox)
+            self.add_geometry()
+        else:
+            self.supobj['type'] = 'Point'
+            self.add_coordinates()
+        self.write_json(fnm)
+        return
+
+    def add_named_crs(self, crs_urn):
+        """ Tag the JSON object with coordinate reference system metadata using
+        a named CRS. The crs_urn should be a valid OGC CRS URN identifier. """
+        # Validate the crs_urn?
+        supobj['crs'] = {'type'         :   'name',
+                         'properties'   :   {'name' :   crs_urn}}
+        return
+
+    def add_bbox(self, bbox=None):
+        """ Tag the JSON object with a bounding box. If *bbox* is None
+        (default), the bounds will be calculated from the data. Otherwise,
+        *bbox* should be in the form (xmin, xmax, ....) for all dimensions.
+        """
+        if bbox is None:
+            if hasattr(self.gpobj, 'get_extents'):
+                bbox = self.gpobj.get_extents()
+            else:
+                raise AttributeError("Type {0} has no 'get_extents' "
+                                     "method".format(type(self.gpobj)))
+        self.supobj['bbox'] = {'bbox'    :   bbox}
+        return
+
+    def add_geometry(self):
+        """ Add geometry subobject. This should be called for writing
+        Multipoint-derived classes. """
+        self.supobj['geometry'] = {'type' : self.typestr}
+        self.add_coordinates(self.supobj['geometry'])
+        self.add_id()
+        if hasattr(self.gpobj.data, 'keys') or \
+                (False in (a is None for a in self.gpobj.data)):
+            self.add_properties()
+        return
+
+    def add_coordinates(self, target=None):
+        """ Add coordinates. Behaviour depends on the type of the geometry
+        object. Target must be None (default) or a dictionary member of the
+        JSON object. """
+        if target is None:
+            target = self.supobj
+
+        if hasattr(self.gpobj, 'get_vertices'):
+            target['coordinates'] = self.gpobj.get_vertices()
+        elif hasattr(self.gpobj, 'get_vertex'):
+            target['coordinates'] = self.gpobj.get_vertex()
+        else:
+            raise guppy.GGeoError('Geometry object has no vertex method')
+        return
+
+    def add_properties(self, target=None):
+        """ Add the data from the geometry object's data attribute. """
+        if target is None:
+            target = self.supobj
+        if hasattr(self.gpobj.data, 'keys'):
+            data = self.gpobj.data
+        else:
+            data = {'point_data': self.gpobj.data}
+        target['properties'] = {}
+        for key in data.keys():
+            target['properties'][key] = data[key]
+        return
+
+    def add_id(self, target=None):
+        """ Add an index field. """
+        if target is None:
+            target = self.supobj
+        target['id'] = range(len(self.gpobj.get_vertices()))
+        return
+
+    def write_json(self, fnm):
+        """ Dump internal dict-object to JSON using the builtin `json` module. """
+        with open(fnm, 'w') as f:
+            json.dump(self.supobj, f, indent=2)
+        return
+
+#class GeoJSONReader:
+
+
+def geojson2csv(fin, fout):
+    """ Convert a JSON file to a CSV. Only properties (attributes) common to
+    all features are retained. The arguments `fin` and `fout` may be either
+    filenames or file-like objects.
+    """
+
+    if hasattr(fin, 'read'):
+        jsdat = json.load(fin)
+    else:
+        with open(fin, 'r') as f:
+            jsdat = json.load(f)
+
+    # Load coordinates and properties
+    X = []
+    Y = []
+    PROP = []
+    try:
+        for feat in jsdat['features']:
+            x, y = feat['geometry']['coordinates']
+            X.append(x)
+            Y.append(y)
+            PROP.append(feat['properties'])
+    except KeyError:
+        sys.stderr.write('invalid geojson schema')
+        traceback.print_exc()
+    except:
+        traceback.print_exc()
+
+    # Find subset of properties that exist in all features
+    if len(PROP) > 1:
+        s = set(PROP[0].keys())
+        common_props = s.intersection(*[set(p.keys()) for p in PROP[1:]])
+    else:
+        common_props = PROP[0].keys()
+
+
+    # Output
+    outstr = 'x,y' + reduce(lambda a,b: a+','+b, common_props) + '\n'   # Construct header
+
+    outstr += reduce(lambda a, b: a+'\n'+b,                         # Combine lines
+                map(lambda x, y, p: str(x) + ',' + str(y) + ',' +   # Construct lines
+                    reduce(lambda a, b: str(a) + ',' + str(b),      # Construct properties
+                        [p[cp] for cp in common_props]),            # Filter spurious props
+                    X, Y, PROP))                                    # Args for line lambda
+
+    try:
+        if hasattr(fout, 'read'):
+            f = fout
+        else:
+            f = open(fout, 'w')
+        f.write(outstr)
+
+    finally:
+        if not hasattr(fout, 'read'):
+            f.close()
+
+    return
+
+
---
+++ vector/_vtk.py
@@ -0,0 +1,114 @@
+""" VTK file driver based on documentation from
+http://vtk.org/VTK/img/file-formats.pdf """
+
+import sys
+import operator
+from xml.etree import ElementTree as ET
+from xml.dom import minidom
+from geo_tools.vector.guppy import *
+
+def mp2vtp(mp_list, fnm, **kwargs):
+    """ Write a list of Multipoint instances to a serial VTK PolyData (.vtp)
+    file.
+
+    kwargs are:
+
+        *vbyteorder*        :   'LittleEndian' or 'BigEndian' to force a byte
+                                order other than the native order
+
+        *write_pointdata*   :   {bool}, default True, to write point attributes
+    """
+
+    vtype = 'PolyData'
+    vbyteorder = kwargs.get('byte_order', 'undef')
+    if vbyteorder == 'undef':
+        if sys.byteorder == 'little':
+            vbyteorder = 'LittleEndian'
+        else:
+            vbyteorder = 'BigEndian'
+    if vbyteorder not in ('LittleEndian', 'BigEndian'):
+        raise Exception("vbyteorder must be 'LittleEndian' or 'BigEndian'")
+    write_pointdata = kwargs.get('write_pointdata', True)
+
+    vversion = str(kwargs.get('version', 0.1))
+
+    xdoc = ET.Element('VTKFile', attrib={'type':vtype,
+                                         'version':vversion,
+                                         'byte_order':vbyteorder})
+
+    xugrid = ET.SubElement(xdoc, vtype)
+
+    if isinstance(mp_list, Multipoint):
+        mp_list = [mp_list]
+
+    for mp in mp_list:
+
+        xpiece = ET.Element('Piece', attrib={'NumberOfPoints':str(len(mp)),
+                                             'NumberOfVerts':str(len(mp))})
+        xugrid.append(xpiece)
+
+        # Point coordinates
+        xpts = ET.Element('Points')
+        xdarray = ET.Element('DataArray',
+                        attrib={'NumberOfComponents':'{0}'.format(mp.rank),
+                                'type':'Float32',
+                                'format':'ascii'})
+        xdarray.text = reduce(lambda a,b: str(a)+' '+str(b),    # Concat points
+                        map(lambda pt:                          # Form points
+                            reduce(lambda a,b: str(a)+' '+str(b), pt),
+                            mp))                                # Concat coords
+
+        xpts.append(xdarray)
+        xpiece.append(xpts)
+
+        # Point data
+        if False in (a is None for a in mp.data) and write_pointdata:
+            if hasattr(mp.data, 'keys'):
+                datakeys = mp.data.keys()
+                datavals = mp.data.values()
+            else:
+                datakeys = ['point_data']
+                datavals = [mp.data]
+
+            xptdata = ET.Element('PointData')
+            for key, vals in zip(datakeys, datavals):
+                if isinstance(vals[0], int):
+                    dtype = 'Int32'
+                elif isinstance(vals[0], float):
+                    dtype = 'Float32'
+                else:
+                    dtype = 'String'
+                xdarray = ET.Element('DataArray', attrib={'type':dtype,
+                                                          'format':'ascii',
+                                                          'Name':str(key)})
+                xdarray.text = reduce(lambda a,b: str(a)+' '+str(b), vals)
+                xptdata.append(xdarray)
+
+            xpiece.append(xptdata)
+
+        # Cell data
+        xcell = ET.Element('Verts')
+        connectivity = ET.Element('DataArray', attrib={'type':'Int32',
+                                                       'Name':'connectivity',
+                                                       'format':'ascii'})
+        connectivity.text = reduce(operator.add,
+                                [str(a)+' ' for a in range(len(mp))])
+
+        offsets = ET.Element('DataArray', attrib={'type':'Int32',
+                                                  'Name':'offsets',
+                                                  'format':'ascii'})
+        #offsets.text = str(len(mp))
+        offsets.text = reduce(operator.add,
+                                [str(a+1)+' ' for a in range(len(mp))])
+
+
+        xcell.append(connectivity)
+        xcell.append(offsets)
+        xpiece.append(xcell)
+
+    pretty_xml = minidom.parseString(ET.tostring(xdoc)).toprettyxml()
+
+    with open(fnm, 'w') as f:
+        f.write(pretty_xml)
+    return
+
