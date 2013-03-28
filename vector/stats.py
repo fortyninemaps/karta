36c468ef9d3028ca7a8ff8e2f9ebccd23bfe5bbc
Nat Wilson <njwilson23@gmail.com>
2013-03-28 10:48:10 -0700

    experimental additions to variogram calc

--- vector/stats.py
+++ vector/stats.py
@@ -1,94 +1,62 @@
 """
-Geostatistics module
-
-Contains variogram calculation and modelling tools
+Variogram estimation and modelling functions
 """
 
-import math
 import numpy as np
-import itertools
+from scipy.spatial.distance import pdist
+import random
 
-def calculate_vario(points, npoints=150, max_dist=None, interval=None):
-    """ Given a list of Points, calculate a spatial variogram. By default,
-    a variogram is quickly estimated. If npoints is None, then the full
-    variogram is calculated.
+def pvariance(A):
+    """ For data `A`, return the pairwise variance (squared differences). This
+    will need to be Cythonized. """
+    n = len(A)
+    V = np.empty(sum(range(n)))
+    for i in range(len(A)):
+        for j in range(i):
+            V[i+j] = (A[i] - A[j])**2
+    return V
+
+def estimate_vario(points, npoints=150, max_dist=None, interval=None):
+    """ Given a Multipoint input, estimate a spatial variogram. By default, a
+    variogram is quickly estimated. If npoints is None, then the full variogram
+    is calculated.
 
     Parameters:
-        points      : list of Point objects
-        npoints     : number of values use for variogram appromixation
-                      if npoints is None, the full variogram is calculated
-        max_dist    : the maximum lag
-        interval    : the lag interval
+    -----------
+    points : a Multipoint instance
+    npoints : number of values use for variogram appromixation. If npoints is
+              None, the full variogram is calculated.
+    max_dist : the maximum lag
+    interval : the lag interval
 
-    Returns
-        bins        : ndarray
-        variogram   : ndarray
+    Returns:
+    --------
+    lags : ndarray
+    variogram : ndarray
     """
-    if isinstance(points, np.ndarray) == False:
-        points = np.array(points)
-
     if npoints is None:
-        npoints = len(points)
+        npoints = len(points.vertices)
 
-    # Get a list of the points that will be calculated
-    ipoints = np.random.permutation(len(points))[:npoints]
+    verts = random.sample(points.vertices, npoints)
+    dist = pdist(verts)
+    vari = pvariance(verts)     # Is this in the right order?
 
-    # Get a pool of points to use for sampling
-    sample_points = np.random.permutation(points)
-
-    # Fill in a datastructure for holding the raw data
-    # This will be a list of lists, npoints long
-    # Each sublist, corresponding to a point in ipoints,
-    # will contain tuples, with every additional point's distance
-    # and the variance
-    V = []
-    for i in ipoints:
-        np.random.shuffle(sample_points)
-        V.append([])
-        pt1 = points[i]
-        i2 = 0
-
-        # Find all other points within max_dist of point
-        for pt2 in sample_points:
-
-            if i2 > npoints:
-                # Have looked through npoints already; move on
-                break
-
-            if pt1 is pt2:
-                # Skip self, so lag != 0
-                pass
-
-            else:
-                d = math.sqrt((pt1.x-pt2.x)**2 + (pt1.y-pt2.y)**2)
-                if (max_dist is None) or (d <= max_dist):
-                    V[-1].append( (d, (pt2.z - pt1.z)**2) )
-
-            i2 += 1
-
-    # Initialize a set of bins for holding variogram values
     if max_dist is None:
-        max_dist = max((i[0] for i in list(itertools.chain(V))))
+        max_dist = dist.max()
 
     if interval is None:
         interval = max_dist / 10.0
-    bins = np.arange(0, max_dist+interval, interval)
 
-    # From the raw data, calculate a variogram
-    variogram = []
-    for b0, bf in zip(bins[:-1], bins[1:]):
+    lags = np.arange(0, max_dist+interval, interval)
+    sigma_variance = np.empty_like(lags)
 
-        p = filter(lambda a: (a[0] >= b0) and (a[0] < bf),
-                   itertools.chain(*V))
-        variance = sum([i[1] for i in p]) / len(p)
-        variogram.append(variance)
-
-    return bins[:-1], np.array(variogram)
-
+    for lag in lags:
+        sigma_variance = np.mean(vari[dist<=lag])
+    return lags, sigma_variance
 
 def sph_func(a, h):
     """ Spherical model function for range *a* and lag *h*. """
-    if h<a:
+    if h < a:
         val = 1.5*h/a - 0.5 * (h/a)**3
     else:
         val = 1
