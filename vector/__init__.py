c55ed305acb01846c700e46d1c7b186bdeb551de
Nat Wilson <njwilson23@gmail.com>
2013-05-14 14:22:38 -0700

    add main read.py funcs to standard import

--- vector/__init__.py
+++ vector/__init__.py
@@ -11,7 +11,9 @@ import shapefile
 import shp_funcs
 
 from guppy import Point, Multipoint, Line, Polygon
-from shp_funcs import read_shapefile, write_shapefile
+from read import read_geojson, read_geojson_features, read_shapefile
+from shp_funcs import write_shapefile
 from gpxparser import GPXParser
 from xyfile import load_xy, xyz2array_reg, array2xyz
+
 
