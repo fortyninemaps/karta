75087d5547fdd310b0e1ac45d4d83ba8f2039e22
Nat Wilson <njwilson23@gmail.com>
2013-05-25 11:15:14 -0700

    add variogram estimator classes to make model composition simpler

--- vector/stats.py
+++ vector/stats.py
@@ -7,25 +7,59 @@ from scipy.spatial.distance import pdist
 from scipy.optimize import minimize
 import random
 
-def pvariance(A):
-    """ For data `A`, return the pairwise variance (squared differences). This
-    will need to be Cythonized. """
-    n = len(A)
-    V = np.empty(sum(range(n)))
-    ind = 0
-    for i in range(len(A)):
-        for j in range(i+1, n):
-            V[ind] = (A[i] - A[j])**2
-            ind += 1
-    return V
+class VariogramFunction(object):
+    """ Base class for variogram functions, which can be composed
+    through summation. """
+
+    def __call__(self, lags):
+        return self.func(lags)
+
+    def __add__(self, other):
+        return ComposedFunction(lambda lags: self(lags) + other(lags))
+
+    def __rmul__(self, other):
+        return ComposedFunction(lambda lags: other * self(lags))
+
+class ComposedFunction(VariogramFunction):
+    """ Type returned from function algebra """
+    def __init__(self, f):
+        self.func = lambda h: f(h)
+        return
+
+class Sph(VariogramFunction):
+    """ Spherical model function """
+    def __init__(self, rng):
+        self.func = lambda h: np.where(h < rng, 1.5*h/rng - 0.5 * (h/rng)**3, 1.0)
+        return
+
+class Gau(VariogramFunction):
+    """ Gaussian model function """
+    def __init__(self, rng):
+        self.func = lambda h: 1.0 - np.exp(-(h**2 / rng**2))
+        return
+
+class Nug(VariogramFunction):
+    """ Nugget effect function """
+    def __init__(self):
+        self.func = lambda h: 1.0
+        return
 
 def fit_model(model, p, lags, variance):
-    """ Fit callable *model* with guess *p* to an experimental variogram
-    given by *lags*, *variance*.
+    """ Fit a variogram model to an experimental variogram.
 
-    *model* should return an estimator function that takes *lags* as an
-    argument
+    Parameters:
+    -----------
+
+    model : function for creating variogram estimators that takes *lags* as an
+            argument, e.g.
+
+        model = lambda p: p[0] * Nug() +  p[1] * Sph(p[2])
+
+    p : array of arguments for *model*
+    lags : variogram offsets
+    variance : variogram variance corresponding to lags
     """
+    lags = np.array(lags)
     obj = lambda p: sum((model(p)(lags) - variance)**2)
     res = minimize(obj, p)
     return res.x
@@ -70,12 +104,15 @@ def estimate_vario(mp, npoints=150, max_dist=None, interval=None):
         sigma_variance[i] = np.mean(vari[band])
     return lags, sigma_variance
 
-def sph_func(a, h):
-    """ Spherical model function for range *a* and lag *h*. """
-    val = np.where(h < a, 1.5*h/a - 0.5 * (h/a)**3, 1.0)
-    return val
+def pvariance(A):
+    """ For data `A`, return the pairwise variance (squared differences). This
+    will need to be Cythonized. """
+    n = len(A)
+    V = np.empty(sum(range(n)))
+    ind = 0
+    for i in range(len(A)):
+        for j in range(i+1, n):
+            V[ind] = (A[i] - A[j])**2
+            ind += 1
+    return V
 
-def gau_func(a, h):
-    """ Gaussian model function for range *a* and lag *h*. """
-    val = 1.0 - np.exp(-(h**2 / a**2))
-    return val
