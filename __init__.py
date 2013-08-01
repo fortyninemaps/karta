859e913838636ae80c12a496b1f73daee69fff4f
Nat Wilson <njwilson23@gmail.com>
2013-08-01 07:05:56 -0400

    remove karta files from gh-pages branch

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Karta is a collection of Python modules for handling vector and raster
-geospatial data.
-"""
-
-import vector
-import raster
-__all__ = ["vector", "raster"]
-
--- tests/test_runner.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-""" Run all tests """
-
-import unittest
-from vector_tests import *
-from raster_tests import *
-
-if __name__ == "__main__":
-    unittest.main()
