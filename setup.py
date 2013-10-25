d1184491c94a225e8e9d8759e6d57e93d8816ca9
Nat Wilson <njwilson23@gmail.com>
2013-10-25 19:12:00 -0400

    setup.py now plays nicely with pip

--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("raster.cfill_sinks", ["raster/cfill_sinks.pyx"]),
-                   Extension("raster.crfuncs", ["raster/crfuncs.pyx"]),
-                   Extension("vector._cvectorgeo", ["vector/_cvectorgeo.pyx"])]
+    name = "karta",
+    version = "0.1",
+    author = "Nat Wilson",
+    #package_dir = {"karta": "src"},
+    packages = ["karta", "karta.vector", "karta.raster"],
+    cmdclass = {"build_ext": build_ext},
+    ext_modules = [Extension("raster.cfill_sinks", ["karta/raster/cfill_sinks.pyx"]),
+                   Extension("raster.crfuncs", ["karta/raster/crfuncs.pyx"]),
+                   Extension("vector._cvectorgeo", ["karta/vector/_cvectorgeo.pyx"])],
 )
