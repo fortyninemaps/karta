3fec83eafabb35b1e4eab754637d8d807520b828
Nat Wilson <njwilson23@gmail.com>
2013-10-26 02:29:10 -0400

    modernize distutils script
    update setup.py according to new cython directions using "cythonize"
    
    add C files to repository so that karta can be built even without cython

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+
+try:
+    from Cython.Build import cythonize
+    USE_CYTHON = True
+except ImportError:
+    USE_CYTHON = False
+
+ext = ".pyx" if USE_CYTHON else ".c"
+
+extensions = [Extension("karta.raster.cfill_sinks", ["karta/raster/cfill_sinks"+ext]),
+              Extension("karta.raster.crfuncs", ["karta/raster/crfuncs"+ext]),
+              Extension("karta.vector._cvectorgeo", ["karta/vector/_cvectorgeo"+ext])]
+
+if USE_CYTHON:
+    extensions = cythonize(extensions)
 
 setup(
     name = "karta",
     version = "0.1",
     author = "Nat Wilson",
-    #package_dir = {"karta": "src"},
     packages = ["karta", "karta.vector", "karta.raster"],
-    cmdclass = {"build_ext": build_ext},
-    ext_modules = [Extension("raster.cfill_sinks", ["karta/raster/cfill_sinks.pyx"]),
-                   Extension("raster.crfuncs", ["karta/raster/crfuncs.pyx"]),
-                   Extension("vector._cvectorgeo", ["karta/vector/_cvectorgeo.pyx"])],
+    ext_modules = extensions
 )
+
