f589ab46005dd9f25afdd2059c6391f039bd0661
Nat Wilson <njwilson23@gmail.com>
2014-04-21 17:12:42 -0400

    refactor md5sum function
    split file logic out into md5sum_file -- it's good to know exactly what a
    function is going to do

--- karta/tests/test_helper.py
+++ karta/tests/test_helper.py
@@ -16,17 +16,18 @@ def md5sum(s, block=32768):
     """ Generate an MD5 hash from a file, string buffer, or filename given by
     *s*. """
     md5 = hashlib.md5()
-    try:
-        if not hasattr(s, 'read'):
-            s = open(s, 'r', encoding='utf-8')
-        while True:
-            data = s.read(block).encode('utf-8')
-            if not data:
-                break
-            md5.update(data)
-    finally:
-        s.close()
+    while True:
+        data = s.read(block)
+        data = data.encode('utf-8')
+        print(data)
+        if not data:
+            break
+        md5.update(data)
     return md5.digest()
+
+def md5sum_file(fnm, block=32768):
+    with open(fnm, 'r') as f:
+        return md5sum(f, block=block)
 
 def tobuffer(s, pos=0):
     """ Place string *s* in a StringIO instance, set to position *pos*. """
