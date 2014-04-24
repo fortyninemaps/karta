7d9fb5c28186247078ee6792a164d0f70237564f
Nat Wilson <njwilson23@gmail.com>
2014-04-23 23:36:25 -0400

    Verify that the source files for extension modules in place, and if not, abort building extensions

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from distutils.core import setup
 from distutils.extension import Extension
 
@@ -9,7 +10,8 @@ try:
 except ImportError:
     USE_CYTHON = False
     include_dirs = []
-    print("Warning: not building accellerated modules")
+    print("Warning: Cython not imported.")
+    print("If C sources exist in the source tree, they will be used.")
 
 ext = ".pyx" if USE_CYTHON else ".c"
 
@@ -19,6 +21,14 @@ extensions = [Extension("karta.raster.cfill_sinks", ["karta/raster/cfill_sinks"+
 
 if USE_CYTHON:
     extensions = cythonize(extensions)
+
+for extension in extensions:
+    if False in (os.path.exists(src) for src in extension.sources):
+        # C-extension sources missing, so don't try to build them
+        extensions = []
+        print("Warning: Extension source not found.")
+        print("Not building accelerated modules")
+        break
 
 setup(
     name = "karta",
