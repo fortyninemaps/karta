eb65afd664e8347273e5a20f34e98bac74afffee
Nat Wilson <njwilson23@gmail.com>
2014-05-24 19:29:06 -0400

    add a couple more crs

--- karta/crs.py
+++ karta/crs.py
@@ -2,11 +2,15 @@
 
 from collections import namedtuple
 
-CRS = namedtuple("CRS", ["shortname", "type", "urn"])
+CRS = namedtuple("CRS", ["name", "type", "urn"])
 
 # List of the available coordinate systems
-LONLAT_WGS84 = CRS("WGS84", "geographical", "urn:ogc:def:crs:EPSG::4326")
 CARTESIAN = CRS("cartesian", "local", "urn:ogc:def:crs:EPSG::5806")
+LONLAT_WGS84 = CRS("lonlat", "geographical", "urn:ogc:def:crs:EPSG::4326")
+UPSNORTH_WGS84 = CRS("UPS North", "projected", "urn:ogc:def:crs:EPSG::32661")
+UPSSOUTH_WGS84 = CRS("UPS South", "projected", "urn:ogc:def:crs:EPSG::32761")
+NSIDCNORTH = CRS("NSIDC Sea Ice Sterographic North", "projected", "urn:ogc:def:crs:EPSG::3411")
+NSIDCSOUTH = CRS("NSIDC Sea Ice Sterographic South", "projected", "urn:ogc:def:crs:EPSG::3412")
 
 
 
