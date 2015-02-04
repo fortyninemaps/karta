83bc55db7b4b34b1d0f8316805947e50b5b2af35
Nat Wilson <njwilson23@gmail.com>
2015-02-04 11:08:41 -0800

    update setup strings and remove beta tag

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import exists
 from setuptools import setup, Extension
 import numpy
 
-VERSION = "0.4.0b5"
+VERSION = "0.4.0"
 
 try:
     from Cython.Build import cythonize
@@ -46,19 +46,18 @@ setup(
     author = "Nat Wilson",
     author_email = "njwilson23@gmail.com",
     packages = ["karta", "karta.vector", "karta.raster"],
-    url = "http://www.ironicmtn.com/karta/",
+    url = "http://www.ironicmtn.com/karta.html",
     description = "Geospatial analysis in Python",
     long_description = """
-Karta - tidy Python package for geospatial computation
-======================================================
+Karta - tidy package for geospatial computation
+===============================================
 
-*Karta* is a Python/Python3 package for geospatial data structures. *Karta*
-serves as a Leatherman for geographic analyses. It provides simple and clean
-vector and raster data types, a selection of geographical analysis methods, and
-the ability to read and write several formats, including GeoJSON, shapefiles,
-and ESRI ASCII.
+*Karta* is a Python/Python3 package for geospatial data structures and
+computation. It provides simple and clean vector and raster data types, a
+selection of geographical analysis methods, and the ability to read and write
+several formats, including GeoJSON, shapefiles, and ESRI ASCII.
 
-The goals of *Karta* is to expose a simple and fast framework for spatial
+The goal of *Karta* is to expose a simple and fast framework for spatial
 analysis.
 
 DOCUMENTATION
@@ -69,131 +68,11 @@ See the `online manual` <http://www.ironicmtn.com/karta/doc/manual/karta-manual.
 The manual can also be built offline using Sphinx by running ``make`` from the
 ``doc/`` directory. The documentation is built from source code docstrings and
 information in the `Wiki <https://github.com/njwilson23/karta/wiki/Tutorial>`__.
-
-CONTENTS AT A GLANCE
---------------------
-
--  vector
-
-   -  geometry: Vector geometry classes (e.g. ``Point``, ``Multipoint``,
-      ``Line``, ``Polygon``) supporting the `Python
-      \_\_geo\_interface\_\_ <https://gist.github.com/sgillies/2217756>`__
-   -  gpx: GPX class for parsing and constructing GPX eXchange files
-   -  geojson: Classes and functions for reading and writing GeoJSON
-   -  shp\_funcs: Shapefile interface through *pyshp*
-   -  xyfile: ASCII table functions
-   -  quadtree: QuadTree implementation
-   -  stats: Geostatistical functions (experimental)
-
--  raster
-
-   -  grid: Basic Grid types, including ``RegularGrid`` and
-      ``WarpedGrid``
-   -  raster: General purpose raster functions
-   -  aaigrid: Grid subclass specifically for reading and writing ESRI
-      ASCII grids
-   -  streamline: Streamline calculation
-   -  flow: Stream flow functions (experimental)
-
--  crs : ``CRS`` and ``CRSRegister`` classes used throughout *Karta*
-
--  tests : unit tests
-
-FORMATS
--------
-
-*Karta* provides a basic working interface to several of common file
-formats. Currently partially-supported are:
-
--  vector
-
-   -  GeoJSON (r,w)
-   -  ESRI Shapefiles via pyshp (r,w)
-   -  GPS eXchange (GPX) (r,w)
-   -  ASCII tables (XYZ) (r,w)
-
--  raster
-
-   -  ESRI ASCII Grid (r,w)
-   -  GeoTiff through GDAL (r)
-   -  USGS DEM (WIP)
-
-INSTALLATION
-------------
-
-The easiest way to install is to use ``pip``.
-
-To install the latest release from PyPI, run
-
-::
-
-    pip install karta
-
-To build from source,
-
-::
-
-    git clone https://github.com/njwilson23/karta.git
-    pip install -r karta/requirements.txt
-    pip install karta/
-
-DEPENDENCIES
-------------
-
-Required
-~~~~~~~~
-
--  Python 2.7+ or Python 3.2+
--  numpy
--  pyshp
--  pyproj (for geodetic calculations)
-
-Optional
-~~~~~~~~
-
--  cython
--  gdal (for geotiff I/O)
--  scipy
-
-Cython is an optional dependency used to speed up select functions. In
-general, enhanced-performance functions will then be called
-automatically when available, otherwise *Karta* will fall back to numpy
-and pure-Python versions.
-
-When installing from PyPI, C source code is provided and will be
-automatically compiled if a suitable compiler is available.
-
-LICENSE
--------
-
-This software is provided under the MIT license.
-
-MIT License:
-~~~~~~~~~~~~
-
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """,
     download_url = "https://github.com/njwilson23/karta/archive/master.zip",
     classifiers = ["Programming Language :: Python",
                    "Programming Language :: Python :: 3",
-                   "Development Status :: 3 - Alpha",
+                   "Development Status :: 3 - Beta",
                    "Topic :: Scientific/Engineering"],
     license = "MIT License",
     ext_modules = extensions
