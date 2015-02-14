41bb97fc3389e55edfe49d90a56b86f136e255c1
Nat Wilson <njwilson23@gmail.com>
2015-02-14 10:47:30 -0800

    rename some test files

--- tests/runtests.py
+++ tests/runtests.py
@@ -4,14 +4,14 @@
 import unittest
 import os
 from test_helper import TESTDIR
-from vector_init_tests import *
-from vector_tests import *
+from crs_tests import *
+from geometry_init_tests import *
+from geometry_tests import *
 from vector_io_tests import *
+from quadtree_tests import *
 from metadata_tests import *
 from raster_tests import *
 from shapefile_tests import *
-from quadtree_tests import *
-from crs_tests import *
 
 if __name__ == "__main__":
 
