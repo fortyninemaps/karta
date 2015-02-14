9916fe2f51ce914bfc60c2eeac0d4106e449a7f7
Nat Wilson <njwilson23@gmail.com>
2015-02-14 11:56:35 -0800

    indicate new page location and remove docs

--- doc/update_manual.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-#
-# assumes that up to date RST sources are in build/html
-# create these from the master branch, using
-#
-# ipython nbconvert tutorial.ipynb --to rst
-
-cp build/html/* manual/ -ruv
-
