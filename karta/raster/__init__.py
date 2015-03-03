f231455ced2cdbfca4a4d184abaa0a9d55977419
Nat Wilson <njwilson23@gmail.com>
2015-03-03 16:52:15 -0500

    rename raster.raster -> raster.misc

--- karta/raster/__init__.py
+++ karta/raster/__init__.py
@@ -4,20 +4,20 @@ Classes for handling raster data.
 
 from . import grid
 from . import aaigrid
-from . import raster
+from . import misc
 
 from .grid import RegularGrid, WarpedGrid
 from .read import read_aai, read_gtiff, aairead, gtiffread
 from .aaigrid import AAIGrid
-from .raster import witch_of_agnesi, peaks, pad, slope, aspect, grad, div
-from .raster import normed_vector_field
+from .misc import witch_of_agnesi, peaks, pad, slope, aspect, grad, div
+from .misc import normed_vector_field
 
 try:
     from .crfuncs import streamline2d
 except ImportError:
     from .streamline import streamline2d
 
-__all__ = ["grid", "aaigrid", "raster",
+__all__ = ["grid", "aaigrid", "misc",
            "RegularGrid", "WarpedGrid",
            "aairead", "gtiffread", "read_aai", "read_gtiff",
            "AAIGrid",
