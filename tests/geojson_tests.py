36e5147d299d51ff3b11bfe21f2f23c0130686c4
Nat Wilson <njwilson23@gmail.com>
2015-09-07 11:54:00 -0400

    test reorganization
    move geojson tests into separate file, rename vector_io_tests to
    misc_io_tests

--- tests/geojson_tests.py
+++ tests/geojson_tests.py
@@ -2,13 +2,11 @@
 
 import unittest
 import os
-import numpy as np
 try:
     from StringIO import StringIO
 except ImportError:
     from io import StringIO
 import json
-import shapely.geometry
 from test_helper import TESTDATA
 
 import karta.vector as vector
@@ -16,97 +14,6 @@ import karta.vector.geojson as geojson
 from karta.vector.geojson import GeoJSONReader, GeoJSONNamedCRS
 from karta.vector.geometry import Point, Multipoint, Line, Polygon
 from karta.crs import LonLatWGS84
-
-class TestGeoInterface(unittest.TestCase):
-
-    def test_point_output(self):
-        p = Point((4, 2))
-        sp = shapely.geometry.shape(p)
-        self.assertEqual(sp.x, p.x)
-        self.assertEqual(sp.y, p.y)
-        return
-
-    def test_multipoint_output(self):
-        p = Multipoint([(4, 2), (3, 5), (3, 2), (7, 3)])
-        sp = shapely.geometry.shape(p)
-        x, y = p.coordinates
-        self.assertEqual(x, tuple([el.x for el in sp]))
-        self.assertEqual(y, tuple([el.y for el in sp]))
-        return
-
-    def test_line_output(self):
-        p = Line([(4, 2), (3, 5), (3, 2), (7, 3)])
-        sp = shapely.geometry.shape(p)
-        x, y = p.coordinates
-        sx, sy = sp.xy
-        self.assertEqual(x, tuple(sx))
-        self.assertEqual(y, tuple(sy))
-        return
-
-    def test_poly_output(self):
-        p = Polygon([(4, 2), (3, 5), (3, 2), (7, 3)])
-        sp = shapely.geometry.shape(p)
-        self.assertEqual(p.bbox, sp.bounds)
-        return
-
-    def test_point_input(self): 
-        sp = shapely.geometry.Point((3,4))
-        p = vector.read.from_shape(sp)
-        self.assertEqual(p.x, sp.x)
-        self.assertEqual(p.y, sp.y)
-        return
-
-    def test_line_input(self): 
-        sp = shapely.geometry.LineString([(3,4), (6,2), (2,5)])
-        p = vector.read.from_shape(sp)
-        x, y = p.coordinates
-        sx, sy = sp.xy
-        self.assertEqual(x, tuple(sx))
-        self.assertEqual(y, tuple(sy))
-        return
-
-    def test_poly_input(self):
-        sp = shapely.geometry.Polygon([(4, 2), (3, 5), (3, 2), (7, 3)])
-        p = vector.read.from_shape(sp)
-        self.assertEqual(p.bbox, sp.bounds)
-        return
-
-    def test_multipoly_input(self):
-        sp1 = shapely.geometry.Polygon([(4, 2), (3, 5), (3, 2), (7, 3)])
-        sp2 = shapely.geometry.Polygon([(7, 3), (9, 7), (2, 7), (2, 0)])
-        smp = shapely.geometry.MultiPolygon([sp1, sp2])
-        p1, p2 = vector.read.from_shape(smp)
-        self.assertEqual(p1.bbox, sp1.bounds)
-        self.assertEqual(p2.bbox, sp2.bounds)
-        return
-
-    def test_multiline_input(self):
-        sp1 = shapely.geometry.LineString([(4, 2), (3, 5), (3, 2), (7, 3)])
-        sp2 = shapely.geometry.LineString([(7, 3), (9, 7), (2, 7), (2, 0)])
-        smp = shapely.geometry.MultiLineString([sp1, sp2])
-        p1, p2 = vector.read.from_shape(smp)
-        x, y = p1.coordinates
-        sx, sy = sp1.xy
-        self.assertEqual(x, tuple(sx))
-        self.assertEqual(y, tuple(sy))
-        x, y = p2.coordinates
-        sx, sy = sp2.xy
-        self.assertEqual(x, tuple(sx))
-        self.assertEqual(y, tuple(sy))
-        return
-
-    def test_feature_input(self):
-        class Pointy(object):
-            __geo_interface__ = {'type': 'Point', 'coordinates': (0.0, 0.0)}
-        class Placemark(object):
-            __geo_interface__ = {
-                'type': 'Feature',
-                'properties': {'name': 'Phoo'},
-                'geometry': Pointy.__geo_interface__ }
-        p = vector.read.from_shape(Placemark())
-        self.assertEqual(p.properties["name"], "Phoo")
-        self.assertEqual(p.vertex, (0.0, 0.0))
-        return
 
 class TestGeoJSONInput(unittest.TestCase):
 
@@ -151,7 +58,7 @@ class TestGeoJSONInput(unittest.TestCase):
         with open(os.path.join(TESTDATA, 'geojson_input/multipolygon.json')) as f:
             reader = GeoJSONReader(f)
         res = reader.parse()
-        self.assertEqual(res.coordinates, 
+        self.assertEqual(res.coordinates,
             [[[[102.0, 2.0], [103.0, 2.0], [103.0, 3.0], [102.0, 3.0], [102.0, 2.0]]],
              [[[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]],
              [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]]]])
@@ -179,7 +86,6 @@ class TestGeoJSONInput(unittest.TestCase):
         self.assertEqual(fc.features[2].properties["scalar"],
                         {"prop0": "value0", "prop1": {"this": "that"}})
         return
-
 
 class TestGeoJSON(unittest.TestCase):
     """ While the test cases TestGeoJSONInput and TestGeoJSONOutput test the
@@ -210,42 +116,41 @@ class TestGeoJSON(unittest.TestCase):
     def test_read_capitols(self):
         path = os.path.join(TESTDATA, "geojson_input/us-capitols.json")
         features = vector.read_geojson(path)
-        names = ['Phoenix, Arizona, United States', 'Sacramento, California, United States', 
-                 'Atlanta, Georgia, United States', 'Indianapolis, Indiana, United States', 
-                 'Helena, Montana, United States', 'Columbus, Ohio, United States', 
-                 'Richmond, Virginia, United States', 'Topeka, Kansas, United States', 
-                 'Boston, Massachusetts, United States', 'Lincoln, Nebraska, United States', 
-                 'Oklahoma City, Oklahoma, United States', 'Juneau, Alaska, United States', 
-                 'Pierre, South Dakota, United States', 'Honolulu, Hawaii, United States', 
+        names = ['Phoenix, Arizona, United States', 'Sacramento, California, United States',
+                 'Atlanta, Georgia, United States', 'Indianapolis, Indiana, United States',
+                 'Helena, Montana, United States', 'Columbus, Ohio, United States',
+                 'Richmond, Virginia, United States', 'Topeka, Kansas, United States',
+                 'Boston, Massachusetts, United States', 'Lincoln, Nebraska, United States',
+                 'Oklahoma City, Oklahoma, United States', 'Juneau, Alaska, United States',
+                 'Pierre, South Dakota, United States', 'Honolulu, Hawaii, United States',
                  'Montgomery, Alabama, United States',
-                 'Little Rock, Arkansas, United States', 'Denver, Colorado, United States', 
-                 'Hartford, Connecticut, United States', 'Dover, Delaware, United States', 
-                 'Washington, District of Columbia, United States', 
-                 'Tallahassee, Florida, United States', 'Boise, Idaho, United States', 
-                 'Springfield, Illinois, United States', 'Des Moines, Iowa, United States', 
-                 'Frankfort, Kentucky, United States', 
-                 'Baton Rouge, Louisiana, United States', 'Augusta, Maine, United States', 
-                 'Annapolis, Maryland, United States', 'Lansing, Michigan, United States', 
-                 'Saint Paul, Minnesota, United States', 
-                 'Jackson, Mississippi, United States', 
-                 'Jefferson City, Missouri, United States', 
-                 'Carson City, Nevada, United States', 
-                 'Concord, New Hampshire, United States', 
-                 'Trenton, New Jersey, United States', 
-                 'Santa Fe, New Mexico, United States', 'Albany, New York, United States', 
-                 'Raleigh, North Carolina, United States', 
-                 'Bismarck, North Dakota, United States', 'Salem, Oregon, United States', 
-                 'Harrisburg, Pennsylvania, United States', 
-                 'Providence, Rhode Island, United States', 
-                 'Columbia, South Carolina, United States', 
-                 'Nashville, Tennessee, United States', 
-                 'Austin, Texas, United States', 'Salt Lake City, Utah, United States', 
-                 'Montpelier, Vermont, United States', 'Olympia, Washington, United States', 
-                 'Charleston, West Virginia, United States', 
+                 'Little Rock, Arkansas, United States', 'Denver, Colorado, United States',
+                 'Hartford, Connecticut, United States', 'Dover, Delaware, United States',
+                 'Washington, District of Columbia, United States',
+                 'Tallahassee, Florida, United States', 'Boise, Idaho, United States',
+                 'Springfield, Illinois, United States', 'Des Moines, Iowa, United States',
+                 'Frankfort, Kentucky, United States',
+                 'Baton Rouge, Louisiana, United States', 'Augusta, Maine, United States',
+                 'Annapolis, Maryland, United States', 'Lansing, Michigan, United States',
+                 'Saint Paul, Minnesota, United States',
+                 'Jackson, Mississippi, United States',
+                 'Jefferson City, Missouri, United States',
+                 'Carson City, Nevada, United States',
+                 'Concord, New Hampshire, United States',
+                 'Trenton, New Jersey, United States',
+                 'Santa Fe, New Mexico, United States', 'Albany, New York, United States',
+                 'Raleigh, North Carolina, United States',
+                 'Bismarck, North Dakota, United States', 'Salem, Oregon, United States',
+                 'Harrisburg, Pennsylvania, United States',
+                 'Providence, Rhode Island, United States',
+                 'Columbia, South Carolina, United States',
+                 'Nashville, Tennessee, United States',
+                 'Austin, Texas, United States', 'Salt Lake City, Utah, United States',
+                 'Montpelier, Vermont, United States', 'Olympia, Washington, United States',
+                 'Charleston, West Virginia, United States',
                  'Madison, Wisconsin, United States', 'Cheyenne, Wyoming, United States']
         self.assertEqual(names, features[0].data.getfield("n"))
         return
-
 
 class TestGeoJSONOutput(unittest.TestCase):
 
@@ -317,63 +222,7 @@ class TestGeoJSONOutput(unittest.TestCase):
                                Point((-134.2, 58.37), crs=LonLatWGS84),
                                Point((-100.3, 44.38), crs=LonLatWGS84)])
         s = capitols.as_geojson()
-
-
-class TestGPX(unittest.TestCase):
-
-    def setUp(self):
-        self.points = [vector.gpx.Point((np.random.random(), np.random.random()),
-                                        {}, {}) for i in range(20)]
-        self.segments = [vector.gpx.Trkseg(self.points, {}, {})]
-        self.tracks = [vector.gpx.Track(self.segments, {}, {})]
-        self.routes = [vector.gpx.Route(self.points, {}, {})]
-
-        self.Point = vector.gpx.Point
-        self.Trkseg = vector.gpx.Trkseg
-        self.Track = vector.gpx.Track
-        self.Route = vector.gpx.Route
-        return
-
-    def test_track_init(self):
-        """ Test initiation of a GPX file containing a single track. """
-        g = vector.gpx.GPX()
-        for i, pt in enumerate(self.points):
-            g.waypts.append(pt)
-        g.tracks = self.tracks
-        g.routes = self.routes
-        return
-
-    def test_add_waypoint(self):
-        waypoint = Point((-80.0, 82.0), 
-                                properties = {"name": "ellesmere", "ele": 100})
-        g = vector.gpx.GPX()
-        g.add_waypoint(waypoint)
-        expected = self.Point((-80.0, 82.0), {"name": "ellesmere",
-                                              "ele": "100"}, {})
-        self.assertEqual(g.waypts[0], expected)
-        return
-
-    def test_add_track(self):
-        track = [Line([(np.random.random(), np.random.random())
-                       for i in range(10)], properties={"name":"segment0"})]
-        g = vector.gpx.GPX()
-        g.add_track(track)
-        expected = self.Track([self.Trkseg(
-                        [self.Point(xy, {}, {}) for xy in track[0].vertices],
-                        {"name":"segment0"}, {})], {}, {})
-        self.assertEqual(g.tracks[0], expected)
-        return
-
-    def test_add_route(self):
-        route = Line([(np.random.random(), np.random.random())
-                      for i in range(10)], properties={"name":"route0"})
-        g = vector.gpx.GPX()
-        g.add_route(route)
-        expected = self.Route([self.Point(xy, {}, {}) for xy in route.vertices],
-                              {"name":"route0"}, {})
-        self.assertEqual(g.routes[0], expected)
-        return
+        self.assertEqual(len(s), 1030)
 
 if __name__ == "__main__":
     unittest.main()
-
