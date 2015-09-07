08e4bf38559a4f7c5fddb6f2ff72d2b06bd04c72
Nat Wilson <njwilson23@gmail.com>
2015-09-07 11:45:49 -0400

    reorganize shp tests and add checks
    - ensure that files get written
    - check CRS of imported files

--- tests/shapefile_tests.py
+++ tests/shapefile_tests.py
@@ -2,7 +2,7 @@ import unittest
 import os
 from os.path import exists, join
 import numpy as np
-from test_helper import TESTDATA
+from test_helper import TESTDIR, TESTDATA
 import datetime
 from copy import copy
 
@@ -30,9 +30,9 @@ class TestShapefile(unittest.TestCase):
                                crs=LonLatWGS84)
 
         self.points3 = [Point((1, 1, 0), crs=LonLatWGS84),
-                       Point((3, 1, 3), crs=LonLatWGS84),
-                       Point((4, 3, 2), crs=LonLatWGS84),
-                       Point((2, 2, -1), crs=LonLatWGS84)]
+                        Point((3, 1, 3), crs=LonLatWGS84),
+                        Point((4, 3, 2), crs=LonLatWGS84),
+                        Point((2, 2, -1), crs=LonLatWGS84)]
 
         self.line3 = Line([(1,5,2),(5,5,-1),(5,1,3),(3,3,1),(1,1,0)], crs=LonLatWGS84)
 
@@ -61,28 +61,40 @@ class TestShapefile(unittest.TestCase):
 
     def test_writepoints(self):
         mp = Multipoint(self.points)
-        mp.to_shapefile("data/points_shp")
+        mp.to_shapefile(os.path.join(TESTDIR, "data/points_shp"))
+        for fnm in ("points_shp.shx", "points_shp.shx", "points_shp.dbf", "points_shp.prj"):
+            self.assertTrue(os.path.isfile(os.path.join(TESTDIR, "data", fnm)))
         return
 
     def test_writeline(self):
-        self.line.to_shapefile("data/line_shp")
+        self.line.to_shapefile(os.path.join(TESTDIR, "data/line_shp"))
+        for fnm in ("line_shp.shx", "line_shp.shx", "line_shp.dbf", "line_shp.prj"):
+            self.assertTrue(os.path.isfile(os.path.join(TESTDIR, "data", fnm)))
         return
 
     def test_writepoly(self):
-        self.polygon.to_shapefile("data/polygon_shp")
+        self.polygon.to_shapefile(os.path.join(TESTDIR, "data/polygon_shp"))
+        for fnm in ("polygon_shp.shx", "polygon_shp.shx", "polygon_shp.dbf", "polygon_shp.prj"):
+            self.assertTrue(os.path.isfile(os.path.join(TESTDIR, "data", fnm)))
         return
 
     def test_writepoints3(self):
         mp = Multipoint(self.points3)
-        mp.to_shapefile("data/pointsz_shp")
+        mp.to_shapefile(os.path.join(TESTDIR, "data/pointsz_shp"))
+        for fnm in ("pointsz_shp.shx", "pointsz_shp.shx", "pointsz_shp.dbf", "pointsz_shp.prj"):
+            self.assertTrue(os.path.isfile(os.path.join(TESTDIR, "data", fnm)))
         return
 
     def test_writeline3(self):
-        self.line3.to_shapefile("data/linez_shp")
+        self.line3.to_shapefile(os.path.join(TESTDIR, "data/linez_shp"))
+        for fnm in ("linez_shp.shx", "linez_shp.shx", "linez_shp.dbf", "linez_shp.prj"):
+            self.assertTrue(os.path.isfile(os.path.join(TESTDIR, "data", fnm)))
         return
 
     def test_writepoly3(self):
-        self.polygon3.to_shapefile("data/polygonz_shp")
+        self.polygon3.to_shapefile(os.path.join(TESTDIR, "data/polygonz_shp"))
+        for fnm in ("polygonz_shp.shx", "polygonz_shp.shx", "polygonz_shp.dbf", "polygonz_shp.prj"):
+            self.assertTrue(os.path.isfile(os.path.join(TESTDIR, "data", fnm)))
         return
 
     def test_write_collection_points(self):
@@ -90,14 +102,20 @@ class TestShapefile(unittest.TestCase):
         mp0 = copy(mp)
         mp1 = copy(mp.shift((4, 2)))
         mp2 = copy(mp.shift((-2, 3)))
-        shp.write_shapefile([mp0, mp1, mp2], "data/points_collection")
+        shp.write_shapefile([mp0, mp1, mp2],
+                            os.path.join(TESTDIR, "data/points_collection"))
+        for fnm in ("points_collection.shx", "points_collection.shx", "points_collection.dbf", "points_collection.prj"):
+            self.assertTrue(os.path.isfile(os.path.join(TESTDIR, "data", fnm)))
         return
 
     def test_write_collection_lines(self):
         line0 = copy(self.line)
         line1 = copy(self.line.shift((4, 2)))
         line2 = copy(self.line.shift((-2, 3)))
-        shp.write_shapefile([line0, line1, line2], "data/line_collection")
+        shp.write_shapefile([line0, line1, line2],
+                            os.path.join(TESTDIR, "data/line_collection"))
+        for fnm in ("line_collection.shx", "line_collection.shx", "line_collection.dbf", "line_collection.prj"):
+            self.assertTrue(os.path.isfile(os.path.join(TESTDIR, "data", fnm)))
         return
 
     def test_dbase_type(self):
@@ -117,38 +135,69 @@ class TestShapefile(unittest.TestCase):
         return
 
     def test_read_points(self):
-        shps = read_shapefile(os.path.join(TESTDATA, "newp"))
-        mp = shps[0]
-        self.assertEqual(mp.vertices,
-                         [(-14.612, 80.50906666666667), (-14.612, 80.50906666666667),
-                          (-14.612, 80.50906666666667), (-13.744733333333333, 80.28181666666667),
-                          (-13.744733333333333, 80.28181666666667), (-13.744733333333333, 80.28181666666667),
-                          (-11.002583333333334, 80.32173333333333), (-11.002583333333334, 80.32173333333333),
-                          (-11.002583333333334, 80.32173333333333), (-11.07225, 80.56316666666666),
-                          (-11.07225, 80.56316666666666)])
-        self.assertEqual(mp.data.getfield("meterno"),
-                         ['IMS1/1', 'IMS2/1', '5952/2', 'IMS4/1', '5953/2', '1963/13',
-                          'IMS5/1', '5213/A', '2121/13', 'IMS3/1', '3613/2'])
-        self.assertEqual(mp.data.getfield("depth_m"),
-                         [73, 143, 247, 86, 147, 250, 74, 142, 235, 150, 248])
-        return
-
-    def test_read_multipoint_attributes(self):
-        mp = read_shapefile(os.path.join(TESTDATA, "shapefiles", "points"))
-        self.assertEqual(mp[0].data.getfield("species"), self.multipoint.data.getfield("species"))
-        return
+        points = read_shapefile(os.path.join(TESTDATA, "shp_input", "points"))
+        mp = points[0]
+        self.assertEqual(len(mp), 4)
+        self.assertTrue("+proj=lonlat" in mp.crs.get_proj4())
+        self.assertTrue("+a=6378137.0" in mp.crs.get_proj4())
+        self.assertTrue("+f=0.00335281" in mp.crs.get_proj4())
+        self.assertEqual(mp.d["species"], ['T. officianale', 'C. tectorum', 'M. alba', 'V. cracca'])
+        self.assertEqual(mp.d["ID"], ['0', '1', '2', '3'])
+        self.assertEqual(mp.coordinates, ((1.0, 3.0, 4.0, 2.0), (1.0, 1.0, 3.0, 2.0)))
 
     def test_read_line(self):
-        line = read_shapefile(os.path.join(TESTDATA, "shapefiles", "line"))[0]
-        self.assertGeomEqual(line, self.line)
+        line = read_shapefile(os.path.join(TESTDATA, "shp_input", "line"))[0]
+        self.assertTrue("+proj=lonlat" in line.crs.get_proj4())
+        self.assertTrue("+a=6378137.0" in line.crs.get_proj4())
+        self.assertTrue("+f=0.00335281" in line.crs.get_proj4())
+        self.assertEqual(line.coordinates, ((1.0, 5.0, 5.0, 3.0, 1.0), (5.0, 5.0, 1.0, 3.0, 1.0)))
         return
 
     def test_read_polygon(self):
-        polygon = read_shapefile(os.path.join(TESTDATA, "shapefiles", "polygon"))[0]
-        self.assertGeomEqual(polygon, self.polygon)
+        polygon = read_shapefile(os.path.join(TESTDATA, "shp_input", "polygon"))[0]
+        self.assertTrue("+proj=lonlat" in polygon.crs.get_proj4())
+        self.assertTrue("+a=6378137.0" in polygon.crs.get_proj4())
+        self.assertTrue("+f=0.00335281" in polygon.crs.get_proj4())
+        self.assertEqual(polygon.coordinates, ((1.0, 5.0, 5.0, 3.0, 1.0), (5.0, 5.0, 1.0, 3.0, 1.0)))
+        return
+
+    def test_read_points_newp(self):
+        # Read a multipoint with a projected cooridnate system
+        shps = read_shapefile(os.path.join(TESTDATA, "shp_input", "newp_nsidc_north"))
+        newp = shps[0]
+
+        proj4 = '+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +a=6378273 +b=6356889.449 +units=m +no_defs  +a=6378273.0 +f=0.00335256126540836'
+        for part in proj4.split():
+            self.assertTrue(part[:8] in newp.crs.get_proj4())
+
+        self.assertEqual(newp.coordinates, ((521236.8297444395,
+                                             521236.8297444395,
+                                             521236.8297444395,
+                                             547490.4452879033,
+                                             547490.4452879033,
+                                             547490.4452879033,
+                                             587584.1578033275,
+                                             587584.1578033275,
+                                             587584.1578033275,
+                                             571828.4918982167,
+                                             571828.4918982167),
+                                            (-888853.1384770898,
+                                             -888853.1384770898,
+                                             -888853.1384770898,
+                                             -902049.3617542256,
+                                             -902049.3617542256,
+                                             -902049.3617542256,
+                                             -871214.0673764511,
+                                             -871214.0673764511,
+                                             -871214.0673764511,
+                                             -850080.914674058,
+                                             -850080.914674058)))
+        self.assertEqual(newp.d["meterno"],
+                         ['IMS1/1', 'IMS2/1', '5952/2', 'IMS4/1', '5953/2', '1963/13',
+                          'IMS5/1', '5213/A', '2121/13', 'IMS3/1', '3613/2'])
+        self.assertEqual(newp.d["depth_m"],
+                         ['73', '143', '247', '86', '147', '250', '74', '142', '235', '150', '248'])
         return
 
 if __name__ == "__main__":
     unittest.main()
-
-
