78c6eca923c4676c89bbfb2e51b6fba1894d97e8
Nat Wilson <njwilson23@gmail.com>
2015-10-15 12:56:14 -0700

    allow other packages to extend karta namespace

--- karta/__init__.py
+++ karta/__init__.py
@@ -3,6 +3,9 @@ Karta is a collection of Python modules for handling vector and raster
 geospatial data.
 """
 
+import pkgutil
+__path__ = pkgutil.extend_path(__path__, __name__)
+
 __version__ = 0.4
 __all__ = ["vector", "raster", "crs", "errors"]
 
