fortyninemaps/karta

commit 33ee4f328e4fcfd7620d710a56db7434fd3c5a57

bugfix: fix pixel offset in grid clipping function
also, fix problem that could cause artifacts when polygon is partially
off-grid

karta/raster/grid.py
@@ -1013,54 +1013,59 @@ def mask_poly(xpoly, ypoly, nx, ny, transform):
         affine transformation describing grid layout and origin
         T == [x0, y0, dx, dy, sx, sy]
     """
-
     mask = np.zeros((ny, nx), dtype=np.int8)
 
-    # find southernmost index (will end on this point)
-    v = max(ypoly)
-    i_end = -1
-    for i in range(len(ypoly)):
+    # find southernmost index (will start and end on this point)
+    v = ypoly[0]
+    i_bot = 0
+    for i in range(1, len(ypoly)):
         if ypoly[i] < v:
             v = ypoly[i]
-            i_end = i
+            i_bot = i
 
-    x0 = xpoly[i_end]
-    y0 = ypoly[i_end]
+    x0 = xpoly[i_bot]
+    y0 = ypoly[i_bot]
 
     ta, tb, tc, td, te, tf = transform
     i0 = int(math.ceil((y0 - min(y0, tb) - tf/tc*(x0 - min(x0, ta))) / (td - tf*te/tc)))
     j0 = int(math.ceil((x0 - min(x0, ta) - te/td*(y0 - min(y0, tb))) / (tc - te*tf/td)))
 
     for el in range(1, len(xpoly)+1):
-        idx = (el + i_end) % len(xpoly)
+        idx = (el + i_bot) % len(xpoly)
         x1 = xpoly[idx]
         y1 = ypoly[idx]
 
-        # Grid coordinates of the segment end points
-        i1 = int(math.ceil((y1 - min(y1, tb) - tf/tc*(x1 - min(x1, ta)))
-                    / (td - tf*te/tc)))
-        j1 = int(math.ceil((x1 - min(x1, ta) - te/td*(y1 - min(y1, tb)))
-                    / (tc - te*tf/td)))
+        # (Unbounded) grid indicies of the segment end points
+        i1 = int(round((y1-tb - tf/tc*(x1-ta)) / (td - tf*te/tc)))
+        j1 = int(round((x1-ta - te/td*(y1-tb)) / (tc - te*tf/td)))
 
-        # if segment is horizontal, ignore
-        if y1 != y0:
+        # If segment is horizontal or off-grid, ignore
+        if ((0 <= i0 < ny) or (0 <= i1 < ny)) and (y1 != y0):
 
             if y1 > y0:     # mark grid cells to the right
 
                 for i in range(i0, i1):
-                    j = int(round((i-i0) * (x1-x0)/(y1-y0) + j0))
-                    mask[max(0, min(i, ny-1)), max(0, min(j, nx-1)):] += 1
+                    if (0 <= i < ny):
+                        j = int(round((i-i0) * (x1-x0)/(y1-y0) + j0))
+                        if j < nx:
+                            mask[i, max(0, j):] += 1
 
-            if y0 > y1:     # unmark grid cells to the right
+            else:           # unmark grid cells to the right
 
                 for i in range(i1, i0):
-                    j = int(round((i-i1) * (x1-x0)/(y1-y0) + j1))
-                    mask[max(0, min(i, ny-1)), max(0, min(j, nx-1)):] -= 1
+                    if (0 <= i < ny):
+                        j = int(round((i-i1) * (x1-x0)/(y1-y0) + j1))
+                        if j < nx:
+                            mask[i, max(0, j):] -= 1
 
         x0 = x1
         y0 = y1
         i0 = i1
         j0 = j1
+
+    # unmark single row of cells to the right
+    if (0 <= i0 < ny) and (j0 < nx):
+        mask[i0, max(0, j0):] += 1
 
     return mask.astype(np.bool)
 

tests/raster_tests.py
@@ -208,7 +208,7 @@ class RegularGridTests(unittest.TestCase):
                                  values=np.arange(1e6).reshape(1000, 1000),
                                  crs=karta.crs.Cartesian)
         masked_grid = grid.mask_by_poly(poly)
-        self.assertEqual(np.nansum(masked_grid.values), 97186599119)
+        self.assertEqual(np.nansum(masked_grid.values), 97047750534)
         return
 
     def test_mask_poly_partial(self):
@@ -236,7 +236,7 @@ class RegularGridTests(unittest.TestCase):
                                  values=np.arange(1e6).reshape(1000, 1000),
                                  crs=karta.crs.Cartesian)
         masked_grid = grid.mask_by_poly([poly, poly2])
-        self.assertEqual(np.nansum(masked_grid.values), 47041821061)
+        self.assertEqual(np.nansum(masked_grid.values), 47080121767)
         return
 
     def test_get_positions(self):

commit 560be92d8a84dc413a3fa63811761f810f0c1d92

another grid clipping fix
33ee4f328e4fcfd7620d710a56db7434fd3c5a57 introduced a couple more small
bugs

karta/raster/grid.py
@@ -1019,20 +1019,20 @@ def mask_poly(xpoly, ypoly, nx, ny, transform):
     y0 = ypoly[i_bot]
 
     ta, tb, tc, td, te, tf = transform
-    i0 = int(math.ceil((y0 - min(y0, tb) - tf/tc*(x0 - min(x0, ta))) / (td - tf*te/tc)))
-    j0 = int(math.ceil((x0 - min(x0, ta) - te/td*(y0 - min(y0, tb))) / (tc - te*tf/td)))
+    i0 = int(round((y0 - min(y0, tb) - tf/tc*(x0 - min(x0, ta))) / (td - tf*te/tc)))
+    j0 = int(round((x0 - min(x0, ta) - te/td*(y0 - min(y0, tb))) / (tc - te*tf/td)))
 
     for el in range(1, len(xpoly)+1):
         idx = (el + i_bot) % len(xpoly)
         x1 = xpoly[idx]
         y1 = ypoly[idx]
 
-        # (Unbounded) grid indicies of the segment end points
+        # (Unbounded) grid indices of the segment end points
         i1 = int(round((y1-tb - tf/tc*(x1-ta)) / (td - tf*te/tc)))
         j1 = int(round((x1-ta - te/td*(y1-tb)) / (tc - te*tf/td)))
 
         # If segment is horizontal or off-grid, ignore
-        if ((0 <= i0 < ny) or (0 <= i1 < ny)) and (y1 != y0):
+        if ((0 <= i0 < ny) and (0 <= i1 < ny)) or (y1 != y0):
 
             if y1 > y0:     # mark grid cells to the right
 
@@ -1054,10 +1054,6 @@ def mask_poly(xpoly, ypoly, nx, ny, transform):
         y0 = y1
         i0 = i1
         j0 = j1
-
-    # unmark single row of cells to the right
-    if (0 <= i0 < ny) and (j0 < nx):
-        mask[i0, max(0, j0):] += 1
 
     return mask.astype(np.bool)
 

tests/raster_tests.py
@@ -209,7 +209,7 @@ class RegularGridTests(unittest.TestCase):
                                  values=np.arange(1e6).reshape(1000, 1000),
                                  crs=karta.crs.Cartesian)
         masked_grid = grid.mask_by_poly(poly)
-        self.assertEqual(np.nansum(masked_grid.values), 97047750534)
+        self.assertEqual(int(np.nansum(masked_grid.values)), 97048730546)
         return
 
     def test_mask_poly_partial(self):
@@ -237,7 +237,7 @@ class RegularGridTests(unittest.TestCase):
                                  values=np.arange(1e6).reshape(1000, 1000),
                                  crs=karta.crs.Cartesian)
         masked_grid = grid.mask_by_poly([poly, poly2])
-        self.assertEqual(np.nansum(masked_grid.values), 47080121767)
+        self.assertEqual(int(np.nansum(masked_grid.values)), 47081206720)
         return
 
     def test_get_positions(self):