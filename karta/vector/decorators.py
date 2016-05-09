145b2eda399a5acd4c6329482619ac3d8f8e03be
Nat Wilson <njwilson23@gmail.com>
2016-05-09 19:00:22 -0400

    check function arguments

--- karta/vector/decorators.py
+++ karta/vector/decorators.py
@@ -6,12 +6,13 @@ def cache_decorator(key):
     Does not update cache when *crs* is specified. """
     def wrapping_func(f):
         def replacement_func(self, *args, **kwargs):
-            if key in self._cache:
-                ret = self._cache[key]
+            if (key in self._cache and
+                    (args == self._cache[key]["args"]) and
+                    (kwargs == self._cache[key]["kwargs"])):
+                ret = self._cache[key]["output"]
             else:
                 ret = f(self, *args, **kwargs)
-                if "crs" not in kwargs:
-                    self._cache[key] = (ret)
+                self._cache[key] = dict(output=ret, args=args, kwargs=kwargs)
             return ret
         return replacement_func
     return wrapping_func
