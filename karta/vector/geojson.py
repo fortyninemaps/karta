4b9ef40dab5c45c2735d50da0a04e986dc8e7c4a
Nat Wilson <njwilson23@gmail.com>
2016-05-19 18:13:33 -0400

    remove geojson2csv function
    untested and would be simpler to do
    
        geojson -> karta geometry type -> csv
    
    rather than having a special case method

--- karta/vector/geojson.py
+++ karta/vector/geojson.py
@@ -361,60 +361,6 @@ def asfixedlist(A):
     else:
         return round(A, 6)
 
-def geojson2csv(fin, fout):
-    """ Convert a JSON file to a CSV. Only properties (attributes) common to
-    all features are retained. The arguments `fin` and `fout` may be either
-    filenames or file-like objects.
-    """
-
-    if hasattr(fin, 'read'):
-        jsdat = json.load(fin)
-    else:
-        with open(fin, 'r') as f:
-            jsdat = json.load(f)
-
-    # Load coordinates and properties
-    X = []
-    Y = []
-    PROP = []
-    for feat in jsdat['features']:
-        x, y = feat['geometry']['coordinates']
-        X.append(x)
-        Y.append(y)
-        PROP.append(feat['properties'])
-
-    # Find subset of properties that exist in all features
-    if len(PROP) > 1:
-        s = set(PROP[0].keys())
-        common_props = s.intersection(*[set(p.keys()) for p in PROP[1:]])
-    else:
-        common_props = PROP[0].keys()
-
-
-    # Output
-    outstr = 'x,y' + \
-             reduce(lambda a,b: a+','+b, common_props) + \
-             '\n'   # Construct header
-
-    outstr += reduce(lambda a, b: a+'\n'+b,                         # Combine lines
-                map(lambda x, y, p: str(x) + ',' + str(y) + ',' +   # Construct lines
-                    reduce(lambda a, b: str(a) + ',' + str(b),      # Construct properties
-                        [p[cp] for cp in common_props]),            # Filter spurious props
-                    X, Y, PROP))                                    # Args for line lambda
-
-    try:
-        if hasattr(fout, 'read'):
-            f = fout
-        else:
-            f = open(fout, 'w')
-        f.write(outstr)
-
-    finally:
-        if not hasattr(fout, 'read'):
-            f.close()
-
-    return
-
 class GeoJSONOutMixin(object):
     """ Mixin class to be added to geometry objects, adding geojson
     functionality.
