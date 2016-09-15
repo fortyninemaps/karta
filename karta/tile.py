616cd3447b36f0a9e34f190a12a6c0f24b5f5b7c
Nat Wilson <njwilson23@gmail.com>
2016-09-15 16:47:13 -0700

    refactor tile functions as methods to new Tile class

--- karta/tile.py
+++ karta/tile.py
@@ -1,11 +1,55 @@
 import math
 from .vector.geometry import Point
 from .crs import LonLatWGS84
-from collections import namedtuple
 
-tile = namedtuple("Tile", ["z", "x", "y"])
+class Tile(object):
 
-def tile_tuple(point, zoom):
+    def __init__(self, z, x, y):
+        self.z = z
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        if not isinstance(other, Tile):
+            return False
+        return (self.z == other.z) and (self.x == other.x) and (self.y == other.y)
+    
+    def __neq__(self, other):
+        return not (self == other)
+
+    def nw_corner(self):
+        """ Return the northwest corner point from a Tile.
+
+        Returns
+        -------
+        Point
+        """
+        z = self.z
+        x = self.x
+        y = self.y
+        n = 2**z
+        lon = float(x)/n * 360.0 - 180.0
+        lat = math.atan(math.sinh(math.pi * (1-2*y/n))) * 180.0 / math.pi
+        return Point((lon, lat), crs=LonLatWGS84)
+
+    @property
+    def bbox(self):
+        """ Return a tuple representing the bounding box of a Tile
+
+        Returns
+        -------
+        tuple
+            (xmin, ymin, xmax, ymax)
+        """
+        z = self.z
+        x = self.x
+        y = self.y
+        pts = [self.nw_corner(),
+               Tile(z, x+1, y).nw_corner(),
+               Tile(z, x, y+1).nw_corner()]
+        return pts[0].x, pts[2].y, pts[1].x, pts[0].y
+
+def tile_from_point(point, zoom):
     """ Return the (z, x, y) locator for an OpenStreetMap tile containing a
     point.
 
@@ -15,6 +59,10 @@ def tile_tuple(point, zoom):
         geographic point to be contained within tile
     zoom : int
         non-negative zoom level (typically 0-18)
+
+    Returns
+    -------
+    Tile
     """
     z = int(zoom)
     dlon = 256
@@ -27,20 +75,4 @@ def tile_tuple(point, zoom):
 
     x = int(x0 // dlon)
     y = int(y0 // dlat)
-    return tile(z, x, y)
-
-def tile_nw_corner(z, x, y):
-    """ Return the northwest corner point from the tile specified by a`
-    tuple.
-    """
-    n = 2**z
-    lon = float(x)/n * 360.0 - 180.0
-    lat = math.atan(math.sinh(math.pi * (1-2*y/n))) * 180.0 / math.pi
-    return Point((lon, lat), crs=LonLatWGS84)
-
-def tile_bbox(z, x, y):
-    """ Return a tuple representing the bounding box of a tile tuple.
-    """
-    pts = [tile_nw_corner(z, x, y), tile_nw_corner(z, x+1, y),
-           tile_nw_corner(z, x, y+1)]
-    return pts[0].x, pts[2].y, pts[1].x, pts[0].y
+    return Tile(z, x, y)
--- tests/tile_tests.py
+++ tests/tile_tests.py
@@ -1,32 +1,32 @@
 import unittest
-from karta.tile import tile, tile_tuple, tile_nw_corner, tile_bbox
+from karta.tile import Tile, tile_from_point
 from karta.vector.geometry import Point
 from karta.crs import LonLatWGS84
 
 class TileTests(unittest.TestCase):
 
     def test_tile_tuple(self):
-        t = tile_tuple(Point((0, 0), crs=LonLatWGS84), 0)
-        self.assertEqual(tuple(t), (0, 0, 0))
+        t = tile_from_point(Point((0, 0), crs=LonLatWGS84), 0)
+        self.assertEqual(t, Tile(0, 0, 0))
         
-        t = tile_tuple(Point((0, 0), crs=LonLatWGS84), 8)
-        self.assertEqual(tuple(t), (8, 128, 128))
+        t = tile_from_point(Point((0, 0), crs=LonLatWGS84), 8)
+        self.assertEqual(t, Tile(8, 128, 128))
 
-        t = tile_tuple(Point((60, -30), crs=LonLatWGS84), 12)
-        self.assertEqual(tuple(t), (12, 2730, 2406))
+        t = tile_from_point(Point((60, -30), crs=LonLatWGS84), 12)
+        self.assertEqual(t, Tile(12, 2730, 2406))
         return
 
     def test_tile_nw_corner(self):
-        pt = tile_nw_corner(0, 0, 0)
+        pt = Tile(0, 0, 0).nw_corner()
         self.assertEqual(pt.x, -180.0)
         self.assertAlmostEqual(pt.y, 85.05112877, places=7)
 
-        pt = tile_nw_corner(1, 1, 1)
+        pt = Tile(1, 1, 1).nw_corner()
         self.assertEqual(pt.vertex, (0, 0))
         return
 
     def test_tile_bbox(self):
-        bbox = tile_bbox(1, 0, 0)
+        bbox = Tile(1, 0, 0).bbox
         self.assertEqual(bbox[0], -180.0)
         self.assertEqual(bbox[2], 0.0)
         self.assertEqual(bbox[1], 0.0)
