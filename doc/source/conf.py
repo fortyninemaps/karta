fortyninemaps/karta

commit a2253b189179f27c24113d6a59598b78a12ea2ce

fix typo in tests and tag version 0.8.2
changes since 0.8.1:

- bugfix for windows (e80401dec9131153f12aeb47c978ffac6cd273c5)
- nodata_value property exposed on RegularGrid
- experimental RegularGrid._align_origin method
- resample functions in raster.crfuncs not take an `na` parameter

doc/source/conf.py
@@ -53,7 +53,7 @@ copyright = u'2016, Nat Wilson'
 # The short X.Y version.
 version = '0.8'
 # The full version, including alpha/beta/rc tags.
-release = '0.8.1'
+release = '0.8.2'
 
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.

karta/version.py
@@ -1,1 +1,1 @@
-__version__ = "0.8.1"
+__version__ = "0.8.2"

tests/grid_tests.py
@@ -132,7 +132,7 @@ class RegularGridTests(unittest.TestCase):
         xx, yy = np.meshgrid(np.linspace(0, 1, 50), np.linspace(0, 1, 30))
         zz = 2.0*xx**2 - 3.0*yy**2
         grid = RegularGrid((27, 53, 5, 5, 0, 0), values=zz)
-        new_grid = grid.align_origin(5, 5, method='linear')
+        new_grid = grid._align_origin(5, 5, method='linear')
         self.assertEqual(new_grid.origin, (25, 55))
         self.assertTrue(np.isnan(new_grid.values[0, 0]))
         return

commit 634041abc5605a8ad8a3153daf81e9d2ef73999f

fix problems introduced to resample by a2253b189179f27c24113d6a59598b78a12ea2ce

doc/source/conf.py
@@ -53,7 +53,7 @@ copyright = u'2016, Nat Wilson'
 # The short X.Y version.
 version = '0.8'
 # The full version, including alpha/beta/rc tags.
-release = '0.8.2'
+release = '0.8.3'
 
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.

karta/raster/grid.py
@@ -756,8 +756,8 @@ class RegularGrid(Grid):
                              "(got {0}, {1})".format(dx, dy))
 
         xmin, xmax, ymin, ymax = self.get_extent()
-        ny = int((ymax - ymin) // (dy / self.resolution[1])) + 1
-        nx = int((xmax - xmin) // (dx / self.resolution[0])) + 1
+        ny = int((ymax - ymin) // dy) + 1
+        nx = int((xmax - xmin) // dx) + 1
 
         t = self._transform
         tnew = (xmin-0.5*dx-0.5*t[4], ymin-0.5*dy-0.5*t[5], dx, dy, t[4], t[5])

karta/version.py
@@ -1,1 +1,1 @@
-__version__ = "0.8.2"
+__version__ = "0.8.3"

tests/grid_tests.py
@@ -147,9 +147,9 @@ class RegularGridTests(unittest.TestCase):
             return RegularGrid((0.0, 0.0, res, res, 0.0, 0.0), values=zz)
 
         # node numbers from a line with extreme edges at [0, 1]
-        g = makegrid(0.0, 1.0-2.0/300, 150, 1.0)
-        sol = makegrid(0.0, 1.0-6.0/300, 50, 3.0)
-        gnew = g.resample(3.0, 3.0, method='nearest')
+        g = makegrid(0.0, 1.0-2.0/300, 150, 2.0)
+        sol = makegrid(0.0, 1.0-6.0/300, 50, 6.0)
+        gnew = g.resample(6.0, 6.0, method='nearest')
         residue = gnew[:,:] - sol[:,:]
         self.assertTrue(np.max(np.abs(residue)) < 1e-12)
         return
@@ -164,9 +164,9 @@ class RegularGridTests(unittest.TestCase):
             return RegularGrid((0.0, 0.0, res, res, 0.0, 0.0), values=zz)
 
         # node numbers from a line with extreme edges at [0, 1]
-        g = makegrid(0.0, 1.0-2.0/300, 150, 1.0)
-        sol = makegrid(0.0, 1.0-6.0/300, 50, 3.0)
-        gnew = g.resample(3.0, 3.0, method='linear')
+        g = makegrid(0.0, 1.0-2.0/300, 150, 2.0)
+        sol = makegrid(0.0, 1.0-6.0/300, 50, 6.0)
+        gnew = g.resample(6.0, 6.0, method='linear')
         residue = gnew[:,:] - sol[:,:]
         self.assertTrue(np.max(np.abs(residue)) < 1e-12)
         return